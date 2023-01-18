895e66d19fd12408c8fedf31daf82616ddb87926
arkteck <arkteck@gmail.com>
2023-01-18 11:33:11 -0500

    Solution for day 2021/01

---
+++ 2021/01/advent202101.py
@@ -0,0 +1,17 @@
+file1 = open('input202101.txt','r')
+lines = file1.readlines()
+lines = [int(line) for line in lines]
+
+count = 0
+for i in range(1, len(lines)):
+  if lines[i] > lines[i - 1]:
+    count += 1
+
+print(count)
+
+count2 = 0
+for i in range(3, len(lines)):
+  if lines[i] > lines[i - 3]:
+    count2 += 1
+
+print(count2)
