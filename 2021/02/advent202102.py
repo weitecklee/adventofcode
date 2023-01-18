c3162352a62fe8f28ea5b47d7340ee97c3ffe3a1
arkteck <arkteck@gmail.com>
2023-01-18 11:43:19 -0500

    Solution for day 2021/02

---
+++ 2021/02/advent202102.py
@@ -0,0 +1,32 @@
+file1 = open('input202102.txt','r')
+lines = file1.readlines()
+
+horizontal = 0
+depth = 0
+
+for line in lines:
+  direction, dist = line.strip().split(' ')
+  if direction == 'forward':
+    horizontal += int(dist)
+  elif direction == 'down':
+    depth += int(dist)
+  elif direction == 'up':
+    depth -= int(dist)
+
+print(horizontal * depth)
+
+horizontal = 0
+depth = 0
+aim = 0
+
+for line in lines:
+  direction, dist = line.strip().split(' ')
+  if direction == 'forward':
+    horizontal += int(dist)
+    depth += int(dist) * aim
+  elif direction == 'down':
+    aim += int(dist)
+  elif direction == 'up':
+    aim -= int(dist)
+
+print(horizontal * depth)
