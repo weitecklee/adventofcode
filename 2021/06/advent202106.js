74d882845e97759196bcddff96d3e5d6992006dd
arkteck <arkteck@gmail.com>
2023-01-20 17:05:32 -0500

    Solution for day 2021/06

---
+++ 2021/06/advent202106.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+let input = fs.readFileSync(path.resolve(__dirname, 'input202106.txt'), 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err)
+  } else {
+    return data;
+  }
+});
+
+input = input.split(',').map(Number);
+
+const fish = new Array(9).fill(0);
+
+for (const n of input) {
+  fish[n]++;
+}
+
+for (let days = 0; days < 80; days++) {
+  const newFish = fish[0];
+  for (let i = 0; i < fish.length - 1; i++) {
+    fish[i] = fish[i + 1];
+  }
+  fish[6] += newFish;
+  fish[8] = newFish;
+}
+
+console.log(fish.reduce((a, b) => a + b));
+
+for (let days = 80; days < 256; days++) {
+  const newFish = fish[0];
+  for (let i = 0; i < fish.length - 1; i++) {
+    fish[i] = fish[i + 1];
+  }
+  fish[6] += newFish;
+  fish[8] = newFish;
+}
+
+console.log(fish.reduce((a, b) => a + b));
