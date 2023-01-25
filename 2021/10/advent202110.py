4110c5a7930191ab7ab19a248e2afc9f514681da
arkteck <arkteck@gmail.com>
2023-01-25 09:42:30 -0500

    Solution for day 2021/10

---
+++ 2021/10/advent202110.py
@@ -0,0 +1,31 @@
+file1 = open('input202110.txt','r')
+lines = file1.readlines()
+
+brackets = {'{': '}', '(': ')', '<': '>', '[': ']'}
+corruptedScores = {')': 3, ']': 57, '}': 1197, '>': 25137}
+corruptedScore = 0
+incompleteScores = {')': 1, ']': 2, '}': 3, '>': 4}
+scores = []
+
+for line in lines:
+  queue = []
+  incomplete = True
+  for bracket in line.strip():
+    if bracket in brackets:
+      queue.append(bracket)
+    elif len(queue) == 0 or brackets[queue.pop()] != bracket:
+      corruptedScore += corruptedScores[bracket]
+      incomplete = False
+      break
+  if incomplete:
+    incompleteScore = 0
+    while len(queue):
+      incompleteScore *= 5
+      incompleteScore += incompleteScores[brackets[queue.pop()]]
+    scores.append(incompleteScore)
+
+scores.sort()
+
+print(corruptedScore)
+print(scores[len(scores) // 2])
+
