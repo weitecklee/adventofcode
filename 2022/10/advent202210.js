7878e92f14722f5d7f8c80ddc3abf30a8e9b2e3d
arkteck <arkteck@gmail.com>
2023-01-30 10:02:49 -0500

    Solution for day 2022/10

---
+++ 2022/10/advent202210.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+let input = fs.readFileSync('input202210.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err)
+  } else {
+    return data;
+  }
+});
+
+input = input.split('\n');
+
+const signal = [1];
+
+for (const line of input) {
+  const last = signal[signal.length - 1];
+  signal.push(last);
+  if (line[0] !== 'n') {
+    const oper = line.split(' ');
+    signal.push(last + Number(oper[1]));
+  }
+}
+
+const qs = [20, 60, 100, 140, 180, 220];
+let sum = 0;
+for (const q of qs) {
+  sum += q * (signal[q - 1]);
+}
+
+console.log(sum);
+
+const screen = new Array(6);
+for (let i = 0; i < 6; i++) {
+  screen[i] = '.'.repeat(40).split('');
+}
+
+for (let i = 0; i < 240; i++) {
+  const x = i % 40;
+  const y = Math.floor(i / 40);
+  if (Math.abs(signal[i] - x) <= 1) {
+    screen[y][x] = '#';
+  }
+}
+for (let i = 0; i < 6; i++) {
+  screen[i] = screen[i].join('');
+  console.log(screen[i]);
+}
