c949ff8f709b3ce8042bf6b1cff28d356b406811
arkteck <arkteck@gmail.com>
2023-02-06 10:40:44 -0500

    Solution for day 2021/12

---
+++ 2021/12/advent202112.py
@@ -0,0 +1,47 @@
+file1 = open('input202112.txt','r')
+lines = file1.readlines()
+
+from collections import defaultdict, deque
+
+connections = defaultdict(list)
+
+for line in lines:
+  a, b = line.strip().split('-')
+  connections[a].append(b)
+  connections[b].append(a)
+
+q = deque([['start', set(['start'])]])
+res = 0
+
+while len(q):
+  cave, visited = q.popleft()
+  for connection in connections[cave]:
+    if connection == 'end':
+      res += 1
+      continue
+    if connection not in visited:
+      newVisited = set(visited)
+      if connection.islower():
+        newVisited.add(connection)
+      q.append([connection, newVisited])
+
+print(res)
+
+q = deque([['start', set(['start']), '']])
+res = 0
+
+while len(q):
+  cave, visited, twice = q.popleft()
+  for connection in connections[cave]:
+    if connection == 'end':
+      res += 1
+      continue
+    if connection not in visited:
+      newVisited = set(visited)
+      if connection.islower():
+        newVisited.add(connection)
+      q.append([connection, newVisited, twice])
+    elif connection != 'start' and twice == '':
+      q.append([connection, visited, connection])
+
+print(res)
