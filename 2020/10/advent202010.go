2d57ad4840394a421d30ee9ad07c45e79e7060b8
arkteck <arkteck@gmail.com>
2023-02-12 10:16:49 -0500

    Solution for day 2020/10

---
+++ 2020/10/advent202010.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("input202010.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := strings.Split(string(data), "\n")
+	parsedInput := []int{}
+	for _, s := range input {
+		n, _ := strconv.Atoi(s)
+		parsedInput = append(parsedInput, n)
+	}
+	parsedInput = append(parsedInput, 0)
+	sort.Ints(parsedInput)
+	parsedInput = append(parsedInput, parsedInput[len(parsedInput)-1]+3)
+	fmt.Println(part1(parsedInput))
+	fmt.Println(part2(parsedInput))
+}
+
+func part1(input []int) int {
+	diff1 := 0
+	diff3 := 0
+	for i := 1; i < len(input); i++ {
+		diff := input[i] - input[i-1]
+		if diff == 1 {
+			diff1++
+		} else if diff == 3 {
+			diff3++
+		}
+	}
+	return diff1 * diff3
+}
+
+func part2(input []int) int {
+	branches := map[int]int{}
+	branches[input[len(input)-1]] = 1
+	for i := len(input) - 2; i >= 0; i-- {
+		branches[input[i]] = branches[input[i]+1] + branches[input[i]+2] + branches[input[i]+3]
+	}
+	return branches[0]
+}
