42412db601fa9e7c7a239a2a032efa2b75b4b417
arkteck <arkteck@gmail.com>
2023-02-22 10:07:42 -0500

    Solution for day 2020/20 part 1

---
+++ 2020/20/advent202020.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("input202020.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := strings.Split(string(data), "\n")
+	parsedInput := parseInput(input)
+	fmt.Println(part1(parsedInput))
+}
+
+func parseInput(input []string) map[string][]int {
+	re := regexp.MustCompile(`\d+`)
+	row := 0
+	edges := map[string][]int{}
+	for row < len(input) {
+		num, _ := strconv.Atoi(re.FindString(input[row]))
+		row++
+		top := input[row]
+		var left strings.Builder
+		var right strings.Builder
+		for row < len(input) && len(input[row]) > 0 {
+			left.WriteString(string(input[row][0]))
+			right.WriteString(string(input[row][len(input[row])-1]))
+			row++
+		}
+		bottom := input[row-1]
+		for _, edge := range []string{top, left.String(), right.String(), bottom} {
+			revEdge := reverse(edge)
+			if _, ok := edges[revEdge]; ok {
+				edges[revEdge] = append(edges[revEdge], num)
+			} else {
+				edges[edge] = append(edges[edge], num)
+			}
+		}
+		row++
+	}
+	return edges
+}
+
+func reverse(s string) string {
+	rev := []rune(s)
+	for i, j := 0, len(rev)-1; i < len(rev)/2; i, j = i+1, j-1 {
+		rev[i], rev[j] = rev[j], rev[i]
+	}
+	return string(rev)
+}
+
+func part1(edges map[string][]int) int {
+	edgePieces := map[int]int{}
+	prod := 1
+	for _, tiles := range edges {
+		if len(tiles) == 1 {
+			edgePieces[tiles[0]]++
+			if edgePieces[tiles[0]] == 2 {
+				prod *= tiles[0]
+			}
+		}
+	}
+	return prod
+}
