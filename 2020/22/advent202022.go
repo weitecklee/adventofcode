d776953359e6e61b5e870805240c6b1b91e3de0f
arkteck <arkteck@gmail.com>
2023-03-09 11:39:43 -0500

    Day 2020/22 part 2 solved

--- 2020/22/advent202022.go
+++ 2020/22/advent202022.go
@@ -15,6 +15,7 @@ func main() {
 	input := strings.Split(string(data), "\n")
 	parsedInput := parseInput(input)
 	fmt.Println(part1(parsedInput))
+	fmt.Println(part2(parsedInput))
 }
 
 type Player struct {
@@ -75,4 +76,60 @@ func part1(players []Player) int {
 		return player1.score()
 	}
 	return player2.score()
+}
+
+func recordHistory(player1 Player, player2 Player) string {
+	history1 := []string{}
+	history2 := []string{}
+	for _, card := range player1.deck {
+		history1 = append(history1, strconv.Itoa(card))
+	}
+	for _, card := range player2.deck {
+		history2 = append(history2, strconv.Itoa(card))
+	}
+	return strings.Join(history1, ",") + "|" + strings.Join(history2, ",")
+}
+
+func recurCombat(player1 Player, player2 Player) (int, int) {
+	history := map[string]bool{}
+	for len(player1.deck) > 0 && len(player2.deck) > 0 {
+		currHistory := recordHistory(player1, player2)
+		if history[currHistory] {
+			return 1, player1.score()
+		}
+		history[currHistory] = true
+		card1 := player1.draw()
+		card2 := player2.draw()
+		if len(player1.deck) >= card1 && len(player2.deck) >= card2 {
+			subPlayer1 := Player{}
+			subPlayer2 := Player{}
+			subdeck1 := make([]int, card1)
+			subdeck2 := make([]int, card2)
+			copy(subdeck1, player1.deck)
+			copy(subdeck2, player2.deck)
+			subPlayer1.deck = subdeck1
+			subPlayer2.deck = subdeck2
+			winner, _ := recurCombat(subPlayer1, subPlayer2)
+			if winner == 1 {
+				player1.add(card1, card2)
+			} else {
+				player2.add(card2, card1)
+			}
+		} else if card1 > card2 {
+			player1.add(card1, card2)
+		} else {
+			player2.add(card2, card1)
+		}
+	}
+	if len(player1.deck) > 0 {
+		return 1, player1.score()
+	}
+	return 2, player2.score()
+}
+
+func part2(players []Player) int {
+	player1 := players[0]
+	player2 := players[1]
+	_, score := recurCombat(player1, player2)
+	return score
 }
