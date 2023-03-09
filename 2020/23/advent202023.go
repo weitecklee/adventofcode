c3779fd012687bcb840f968e2b4496f1df5b016c
arkteck <arkteck@gmail.com>
2023-03-09 16:01:22 -0500

    Day 2020/23 part 1 modified to use doubly linked list

--- 2020/23/advent202023.go
+++ 2020/23/advent202023.go
@@ -33,43 +33,49 @@ func contains(arr []int, n int) bool {
 	return false
 }
 
-func move(cups []int) []int {
-	curr := cups[0]
-	pickup := cups[1:4]
+func move(cups [10][2]int, curr int) ([10][2]int, int) {
+	next3 := []int{}
+	tmp := curr
+	for i := 0; i < 3; i++ {
+		next3 = append(next3, cups[tmp][1])
+		tmp = cups[tmp][1]
+	}
 	dest := curr - 1
 	if dest == 0 {
 		dest = 9
 	}
-	for contains(pickup, dest) {
+	for contains(next3, dest) {
 		dest--
 		if dest == 0 {
 			dest = 9
 		}
 	}
-	cups2 := []int{}
-	for _, n := range cups[4:] {
-		cups2 = append(cups2, n)
-		if n == dest {
-			cups2 = append(cups2, pickup...)
-		}
-	}
-	return append(cups2, curr)
+	cups[curr][1] = cups[next3[2]][1]
+	oldNext := cups[dest][1]
+	cups[dest][1] = next3[0]
+	cups[next3[2]][1] = oldNext
+	return cups, cups[curr][1]
 }
 
 func part1(input []int) string {
-	for i := 0; i < 100; i++ {
-		input = move(input)
+	cups := [10][2]int{} // doubly linked list approach, cups[i] = [previous, next] for cup i
+	cups[input[0]][0] = input[8]
+	cups[input[0]][1] = input[1]
+	cups[input[8]][0] = input[7]
+	cups[input[8]][1] = input[0]
+	for i := 1; i < len(input)-1; i++ {
+		cups[input[i]][0] = input[i-1]
+		cups[input[i]][1] = input[i+1]
 	}
-	idx := 0
-	for input[idx] != 1 {
-		idx++
+	curr := input[0]
+	for i := 0; i < 100; i++ {
+		cups, curr = move(cups, curr)
 	}
 	ret := ""
-	for i := idx + 1; i < len(input); i++ {
-		ret += strconv.Itoa(input[i])
-	}
-	for i := 0; i < idx; i++ {
-		ret += strconv.Itoa(input[i])
+	tmp := 1
+	for cups[tmp][1] != 1 {
+		ret += strconv.Itoa(cups[tmp][1])
+		tmp = cups[tmp][1]
 	}
 	return ret
 }
