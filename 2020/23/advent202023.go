f2a031577779388a77ff0e03d98cc32b92da8cd6
arkteck <arkteck@gmail.com>
2023-03-09 20:17:54 -0500

    modify to use container/ring for circular list

--- 2020/23/advent202023.go
+++ 2020/23/advent202023.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/ring"
 	"fmt"
 	"os"
 	"strconv"
@@ -24,58 +25,48 @@ func parseInput(input string) []int {
 	return arr
 }
 
-func contains(arr []int, n int) bool {
-	for _, v := range arr {
-		if v == n {
-			return true
+func game(cups *ring.Ring, turns int) *ring.Ring {
+	cupMap := map[int]*ring.Ring{} // package Ring implements circular lists, how convenient!
+	max := cups.Len()
+	for i := 0; i < cups.Len(); i++ {
+		cupMap[cups.Value.(int)] = cups
+		cups = cups.Next()
+	}
+	for i := 0; i < turns; i++ {
+		removed := cups.Unlink(3)
+		dest := cups.Value.(int)
+		for {
+			dest--
+			if dest == 0 {
+				dest = max
+			}
+			inRemoved := false
+			removed.Do(func(val any) {
+				if val.(int) == dest {
+					inRemoved = true
+				}
+			})
+			if !inRemoved {
+				break
+			}
 		}
+		cupMap[dest].Link(removed)
+		cups = cups.Next()
 	}
-	return false
-}
-
-func move(cups [10][2]int, curr int) ([10][2]int, int) {
-	next3 := []int{}
-	tmp := curr
-	for i := 0; i < 3; i++ {
-		next3 = append(next3, cups[tmp][1])
-		tmp = cups[tmp][1]
-	}
-	dest := curr - 1
-	if dest == 0 {
-		dest = 9
-	}
-	for contains(next3, dest) {
-		dest--
-		if dest == 0 {
-			dest = 9
-		}
-	}
-	cups[curr][1] = cups[next3[2]][1]
-	oldNext := cups[dest][1]
-	cups[dest][1] = next3[0]
-	cups[next3[2]][1] = oldNext
-	return cups, cups[curr][1]
+	return cupMap[1]
 }
 
 func part1(input []int) string {
-	cups := [10][2]int{} // doubly linked list approach, cups[i] = [previous, next] for cup i
-	cups[input[0]][0] = input[8]
-	cups[input[0]][1] = input[1]
-	cups[input[8]][0] = input[7]
-	cups[input[8]][1] = input[0]
-	for i := 1; i < len(input)-1; i++ {
-		cups[input[i]][0] = input[i-1]
-		cups[input[i]][1] = input[i+1]
+	cups := ring.New(len(input))
+	for _, n := range input {
+		cups.Value = n
+		cups = cups.Next()
 	}
-	curr := input[0]
-	for i := 0; i < 100; i++ {
-		cups, curr = move(cups, curr)
-	}
+	cups = game(cups, 100)
 	ret := ""
-	tmp := 1
-	for cups[tmp][1] != 1 {
-		ret += strconv.Itoa(cups[tmp][1])
-		tmp = cups[tmp][1]
+	for i := 0; i < 8; i++ {
+		cups = cups.Next()
+		ret += strconv.Itoa(cups.Value.(int))
 	}
 	return ret
 }
