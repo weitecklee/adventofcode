8c992ae80a0eceb4a023056e147d841a0ff13440
arkteck <arkteck@gmail.com>
2023-03-19 14:46:33 -0400

    Day 2017/01 part 1 solved

---
+++ 2017/01/advent201701.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(data)
+	parsedInput := parseInput(input)
+	fmt.Println(part1(parsedInput))
+}
+
+func parseInput(input string) []int {
+	res := []int{}
+	for _, c := range input {
+		n, _ := strconv.Atoi(string(c))
+		res = append(res, n)
+	}
+	return res
+}
+
+func part1(input []int) int {
+	res := 0
+	if input[0] == input[len(input)-1] {
+		res = input[0]
+	}
+	for i := 0; i < len(input)-1; i++ {
+		if input[i] == input[i+1] {
+			res += input[i]
+		}
+	}
+	return res
+}
