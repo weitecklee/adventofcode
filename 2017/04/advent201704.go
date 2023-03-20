1b3376529fbafed6a2b56c761d61b87f3b311c7a
arkteck <arkteck@gmail.com>
2023-03-19 20:36:56 -0400

    Day 2017/04 part 2 solved

--- 2017/04/advent201704.go
+++ 2017/04/advent201704.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -13,6 +14,7 @@ func main() {
 	}
 	input := strings.Split(string(data), "\n")
 	fmt.Println(part1(input))
+	fmt.Println(part2(input))
 }
 
 func part1(input []string) int {
@@ -27,6 +29,31 @@ func part1(input []string) int {
 				break
 			}
 			wordMap[word] = true
+		}
+		if valid {
+			res++
+		}
+	}
+	return res
+}
+
+func part2(input []string) int {
+	res := 0
+	for _, row := range input {
+		words := strings.Split(row, " ")
+		wordMap := map[string]bool{}
+		valid := true
+		for _, word := range words {
+			chars := []rune(word)
+			sort.Slice(chars, func(i, j int) bool {
+				return chars[i] < chars[j]
+			})
+			sortedWord := string(chars)
+			if wordMap[sortedWord] {
+				valid = false
+				break
+			}
+			wordMap[sortedWord] = true
 		}
 		if valid {
 			res++
