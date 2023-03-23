e8ed0d8e6f5ffff28add830e65c13adf4e31d1d9
arkteck <arkteck@gmail.com>
2023-03-23 10:09:27 -0400

    Day 2017/12 part 1 solved

---
+++ 2017/12/advent201712.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := strings.Split(string(data), "\n")
+	fmt.Println(part1(parseInput(input)))
+
+}
+
+func parseInput(input []string) [][]int {
+	parsed := [][]int{}
+	re := regexp.MustCompile(`\d+`)
+	for _, line := range input {
+		matches := re.FindAllString(line, -1)
+		tmp := []int{}
+		for _, match := range matches {
+			n, _ := strconv.Atoi(match)
+			tmp = append(tmp, n)
+		}
+		parsed = append(parsed, tmp)
+	}
+	return parsed
+}
+
+func part1(input [][]int) int {
+	visited := map[int]bool{}
+	visited[0] = true
+	queue := []int{0}
+	for len(queue) > 0 {
+		pipe := queue[0]
+		connectingPipes := input[pipe][1:]
+		for _, connectingPipe := range connectingPipes {
+			if !visited[connectingPipe] {
+				queue = append(queue, connectingPipe)
+				visited[connectingPipe] = true
+			}
+		}
+		queue = queue[1:]
+	}
+	return len(visited)
+}
