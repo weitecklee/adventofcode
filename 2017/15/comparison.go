6d5a585237c97f7dbe4b2f6069aaa4366378a877
arkteck <arkteck@gmail.com>
2023-03-24 15:33:50 -0400

    Using bitwise & is faster than modulo

--- 2017/15/comparison.go
+++ 2017/15/comparison.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -15,6 +16,16 @@ func main() {
 	parsedInput := parseInput(strings.Split(string(data), "\n"))
 	fmt.Println(part1(parsedInput))
 	fmt.Println(part2(parsedInput))
+	fmt.Println(part1v2(parsedInput))
+	fmt.Println(part2v2(parsedInput))
+}
+
+func track(msg string) (string, time.Time) {
+	return msg, time.Now()
+}
+
+func duration(msg string, start time.Time) {
+	fmt.Printf("%v: %v\n", msg, time.Since(start))
 }
 
 func parseInput(input []string) []int {
@@ -28,6 +39,24 @@ func parseInput(input []string) []int {
 }
 
 func part1(input []int) int {
+	defer duration(track("part1"))
+	count := 0
+	factorA := 16807
+	factorB := 48271
+	genA := input[0]
+	genB := input[1]
+	for i := 0; i < 40000000; i++ {
+		genA = (genA * factorA) % 2147483647
+		genB = (genB * factorB) % 2147483647
+		if genA%65536 == genB%65536 {
+			count++
+		}
+	}
+	return count
+}
+
+func part1v2(input []int) int {
+	defer duration(track("part1 v2"))
 	count := 0
 	factorA := 16807
 	factorB := 48271
@@ -54,6 +83,24 @@ func generator(val int, f int, m int) int {
 }
 
 func part2(input []int) int {
+	defer duration(track("part2"))
+	count := 0
+	factorA := 16807
+	factorB := 48271
+	genA := input[0]
+	genB := input[1]
+	for i := 0; i < 5000000; i++ {
+		genA = generator(genA, factorA, 4)
+		genB = generator(genB, factorB, 8)
+		if genA%65536 == genB%65536 {
+			count++
+		}
+	}
+	return count
+}
+
+func part2v2(input []int) int {
+	defer duration(track("part2 v2"))
 	count := 0
 	factorA := 16807
 	factorB := 48271
