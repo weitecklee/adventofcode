b9783644246bbb960bafc2c63f9b489840417864
arkteck <arkteck@gmail.com>
2023-03-25 10:33:50 -0400

    Day 2017/16 part 2 solved

--- 2017/16/advent201716.go
+++ 2017/16/advent201716.go
@@ -14,6 +14,40 @@ func main() {
 	}
 	input := strings.Split(string(data), ",")
 	fmt.Println(part1(input))
+	fmt.Println(part2(input))
+}
+
+func dance(dancers string, step string) string {
+	dancers2 := ""
+	if step[0] == 's' {
+		spin, _ := strconv.Atoi(step[1:])
+		dancers2 = dancers[(len(dancers)-spin):] + dancers[:(len(dancers)-spin)]
+	} else if step[0] == 'x' {
+		swaps := strings.Split(step[1:], "/")
+		swap1, _ := strconv.Atoi(swaps[0])
+		swap2, _ := strconv.Atoi(swaps[1])
+		for i := 0; i < len(dancers); i++ {
+			if i == swap1 {
+				dancers2 += string(dancers[swap2])
+			} else if i == swap2 {
+				dancers2 += string(dancers[swap1])
+			} else {
+				dancers2 += string(dancers[i])
+			}
+		}
+	} else {
+		swaps := strings.Split(step[1:], "/")
+		for i := 0; i < len(dancers); i++ {
+			if string(dancers[i]) == swaps[0] {
+				dancers2 += swaps[1]
+			} else if string(dancers[i]) == swaps[1] {
+				dancers2 += swaps[0]
+			} else {
+				dancers2 += string(dancers[i])
+			}
+		}
+	}
+	return dancers2
 }
 
 func part1(input []string) string {
@@ -23,36 +57,36 @@ func part1(input []string) string {
 		dancers += string(i + 97)
 	}
 	for _, step := range input {
-		dancers2 := ""
-		if step[0] == 's' {
-			spin, _ := strconv.Atoi(step[1:])
-			dancers2 = dancers[(n-spin):] + dancers[:(n-spin)]
-		} else if step[0] == 'x' {
-			swaps := strings.Split(step[1:], "/")
-			swap1, _ := strconv.Atoi(swaps[0])
-			swap2, _ := strconv.Atoi(swaps[1])
-			for i := 0; i < n; i++ {
-				if i == swap1 {
-					dancers2 += string(dancers[swap2])
-				} else if i == swap2 {
-					dancers2 += string(dancers[swap1])
-				} else {
-					dancers2 += string(dancers[i])
-				}
-			}
-		} else {
-			swaps := strings.Split(step[1:], "/")
-			for i := 0; i < n; i++ {
-				if string(dancers[i]) == swaps[0] {
-					dancers2 += swaps[1]
-				} else if string(dancers[i]) == swaps[1] {
-					dancers2 += swaps[0]
-				} else {
-					dancers2 += string(dancers[i])
-				}
-			}
+		dancers = dance(dancers, step)
+	}
+	return dancers
+}
+
+func part2(input []string) string {
+	n := 16
+	dancers := ""
+	for i := 0; i < n; i++ {
+		dancers += string(i + 97)
+	}
+	history := map[string]int{}
+	i := 0
+	j := 0
+	for {
+		history[strconv.Itoa(j)+dancers] = i
+		dancers = dance(dancers, input[j])
+		i++
+		j++
+		if j > len(input)-1 {
+			j = 0
 		}
-		dancers = dancers2
+		if _, ok := history[strconv.Itoa(j)+dancers]; ok {
+			break
+		}
+	}
+	period := i - history[strconv.Itoa(j)+dancers]
+	leftover := 1000000000 % period
+	for k := 0; k < leftover; k++ {
+		dancers = dance(dancers, input[(j+k)%len(input)])
 	}
 	return dancers
 }
