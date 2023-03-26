a2a5904c9617317aef4ada3da5f1a2f185a56cc6
arkteck <arkteck@gmail.com>
2023-03-26 14:43:44 -0400

    Day 2017/18 part 2 solved

--- 2017/18/advent201718.go
+++ 2017/18/advent201718.go
@@ -13,7 +13,9 @@ func main() {
 		panic(err)
 	}
 	input := strings.Split(string(data), "\n")
-	fmt.Println(part1(parseInput(input)))
+	instructions := parseInput(input)
+	fmt.Println(part1(instructions))
+	fmt.Println(part2(instructions))
 }
 
 type Instruction struct {
@@ -95,4 +97,89 @@ func part1(instructions *[]Instruction) int {
 		i, snd = execute(i, snd, instructions, &registers)
 	}
 	return snd
+}
+
+func program(prog int, i int, snd0 *[]int, snd1 *[]int, instructions *[]Instruction, registers *map[string]int, count int) (int, int) {
+	instruction := (*instructions)[i]
+	switch instruction.action {
+	case "snd":
+		if prog == 0 {
+			*snd0 = append(*snd0, (*registers)[instruction.subject])
+		} else {
+			*snd1 = append(*snd1, (*registers)[instruction.subject])
+			count++
+		}
+	case "set":
+		if instruction.valueString != "" {
+			(*registers)[instruction.subject] = (*registers)[instruction.valueString]
+		} else {
+			(*registers)[instruction.subject] = instruction.valueInt
+		}
+	case "add":
+		if instruction.valueString != "" {
+			(*registers)[instruction.subject] += (*registers)[instruction.valueString]
+		} else {
+			(*registers)[instruction.subject] += instruction.valueInt
+		}
+	case "mul":
+		if instruction.valueString != "" {
+			(*registers)[instruction.subject] *= (*registers)[instruction.valueString]
+		} else {
+			(*registers)[instruction.subject] *= instruction.valueInt
+		}
+	case "mod":
+		if instruction.valueString != "" {
+			(*registers)[instruction.subject] %= (*registers)[instruction.valueString]
+		} else {
+			(*registers)[instruction.subject] %= instruction.valueInt
+		}
+	case "rcv":
+		if prog == 0 {
+			(*registers)[instruction.subject] = (*snd1)[0]
+			*snd1 = (*snd1)[1:]
+		} else {
+			(*registers)[instruction.subject] = (*snd0)[0]
+			*snd0 = (*snd0)[1:]
+		}
+	case "jgz":
+		if (*registers)[instruction.subject] > 0 {
+			if instruction.valueString != "" {
+				i += (*registers)[instruction.valueString] - 1
+			} else {
+				i += instruction.valueInt - 1
+			}
+		}
+	}
+	return i + 1, count
+}
+
+func part2(instructions *[]Instruction) int {
+	registers0 := map[string]int{}
+	registers1 := map[string]int{}
+	registers0["1"] = 1 // easiest way to account for input line "jgz 1 3",
+	registers1["1"] = 1 // only occurrence of subject not pointing to register
+	registers1["p"] = 1
+	snd0 := []int{}
+	snd1 := []int{}
+	count := 0
+	i := 0
+	j := 0
+loop:
+	for i < len(*instructions) && i >= 0 && j < len(*instructions) && j >= 0 {
+		for (*instructions)[i].action == "rcv" && len(snd1) == 0 {
+			if (*instructions)[j].action == "rcv" && len(snd0) == 0 {
+				break loop
+			}
+			j, count = program(1, j, &snd0, &snd1, instructions, &registers1, count)
+		}
+		for (*instructions)[j].action == "rcv" && len(snd0) == 0 {
+			if (*instructions)[i].action == "rcv" && len(snd1) == 0 {
+				break loop
+			}
+			i, count = program(0, i, &snd0, &snd1, instructions, &registers0, count)
+		}
+		i, count = program(0, i, &snd0, &snd1, instructions, &registers0, count)
+		j, count = program(1, j, &snd0, &snd1, instructions, &registers1, count)
+	}
+	return count
 }
