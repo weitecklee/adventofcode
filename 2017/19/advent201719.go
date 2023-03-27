512a85f3292622a98529e3b800eee638007958b2
arkteck <arkteck@gmail.com>
2023-03-27 11:56:09 -0400

    Day 2017/19 part 1 solved

---
+++ 2017/19/advent201719.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := strings.Split(string(data), "\n")
+	fmt.Println(part1(input))
+}
+
+func part1(input []string) string {
+	pos := [2]int{0, 0}
+	pos[1] = strings.Index(input[0], "|")
+	dir := [2]int{1, 0}
+	res := ""
+	re := regexp.MustCompile(`[A-Z]`)
+loop:
+	for {
+		pos[0] += dir[0]
+		pos[1] += dir[1]
+		curr := string(input[pos[0]][pos[1]])
+		if re.MatchString(curr) {
+			res += curr
+			continue
+		}
+		switch curr {
+		case "+":
+			if dir[0] == 0 {
+				dir[1] = 0
+				if pos[0] > 0 && pos[1] < len(input[pos[0]-1]) && input[pos[0]-1][pos[1]] == '|' {
+					dir[0] = -1
+				} else {
+					dir[0] = 1
+				}
+			} else {
+				dir[0] = 0
+				if pos[1] > 0 && input[pos[0]][pos[1]-1] == '-' {
+					dir[1] = -1
+				} else {
+					dir[1] = 1
+				}
+			}
+		case " ":
+			break loop
+		}
+	}
+	return res
+}
