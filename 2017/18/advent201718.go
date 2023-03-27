bf4dea31983c1e1611792d76d2d0f7bff9ec2693
arkteck <arkteck@gmail.com>
2023-03-27 10:34:29 -0400

    Refactor to use coroutines and channels. Implement one second wait for rcv before exiting

--- 2017/18/advent201718.go
+++ 2017/18/advent201718.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -99,87 +101,71 @@ func part1(instructions *[]Instruction) int {
 	return snd
 }
 
-func program(prog int, i int, snd0 *[]int, snd1 *[]int, instructions *[]Instruction, registers *map[string]int, count int) (int, int) {
-	instruction := (*instructions)[i]
-	switch instruction.action {
-	case "snd":
-		if prog == 0 {
-			*snd0 = append(*snd0, (*registers)[instruction.subject])
-		} else {
-			*snd1 = append(*snd1, (*registers)[instruction.subject])
-			count++
-		}
-	case "set":
-		if instruction.valueString != "" {
-			(*registers)[instruction.subject] = (*registers)[instruction.valueString]
-		} else {
-			(*registers)[instruction.subject] = instruction.valueInt
-		}
-	case "add":
-		if instruction.valueString != "" {
-			(*registers)[instruction.subject] += (*registers)[instruction.valueString]
-		} else {
-			(*registers)[instruction.subject] += instruction.valueInt
-		}
-	case "mul":
-		if instruction.valueString != "" {
-			(*registers)[instruction.subject] *= (*registers)[instruction.valueString]
-		} else {
-			(*registers)[instruction.subject] *= instruction.valueInt
-		}
-	case "mod":
-		if instruction.valueString != "" {
-			(*registers)[instruction.subject] %= (*registers)[instruction.valueString]
-		} else {
-			(*registers)[instruction.subject] %= instruction.valueInt
-		}
-	case "rcv":
-		if prog == 0 {
-			(*registers)[instruction.subject] = (*snd1)[0]
-			*snd1 = (*snd1)[1:]
-		} else {
-			(*registers)[instruction.subject] = (*snd0)[0]
-			*snd0 = (*snd0)[1:]
-		}
-	case "jgz":
-		if (*registers)[instruction.subject] > 0 {
+func program(prog int, inChan chan int, outChan chan int, instructions *[]Instruction, count *int, wg *sync.WaitGroup) {
+	registers := map[string]int{}
+	registers["1"] = 1 // easiest way to account for input line "jgz 1 3",
+	registers["p"] = prog
+	i := 0
+	for i >= 0 && i < len(*instructions) {
+		instruction := (*instructions)[i]
+		switch instruction.action {
+		case "snd":
+			outChan <- registers[instruction.subject]
+			if prog == 1 {
+				*count++
+			}
+		case "set":
 			if instruction.valueString != "" {
-				i += (*registers)[instruction.valueString] - 1
+				registers[instruction.subject] = registers[instruction.valueString]
 			} else {
-				i += instruction.valueInt - 1
+				registers[instruction.subject] = instruction.valueInt
+			}
+		case "add":
+			if instruction.valueString != "" {
+				registers[instruction.subject] += registers[instruction.valueString]
+			} else {
+				registers[instruction.subject] += instruction.valueInt
+			}
+		case "mul":
+			if instruction.valueString != "" {
+				registers[instruction.subject] *= registers[instruction.valueString]
+			} else {
+				registers[instruction.subject] *= instruction.valueInt
+			}
+		case "mod":
+			if instruction.valueString != "" {
+				registers[instruction.subject] %= registers[instruction.valueString]
+			} else {
+				registers[instruction.subject] %= instruction.valueInt
+			}
+		case "rcv":
+			select {
+			case registers[instruction.subject] = <-inChan:
+			case <-time.After(time.Second):
+				wg.Done()
+				return
+			}
+		case "jgz":
+			if registers[instruction.subject] > 0 {
+				if instruction.valueString != "" {
+					i += registers[instruction.valueString] - 1
+				} else {
+					i += instruction.valueInt - 1
+				}
 			}
 		}
+		i++
 	}
-	return i + 1, count
 }
 
 func part2(instructions *[]Instruction) int {
-	registers0 := map[string]int{}
-	registers1 := map[string]int{}
-	registers0["1"] = 1 // easiest way to account for input line "jgz 1 3",
-	registers1["1"] = 1 // only occurrence of subject not pointing to register
-	registers1["p"] = 1
-	snd0 := []int{}
-	snd1 := []int{}
+	snd0 := make(chan int, 10000)
+	snd1 := make(chan int, 10000)
 	count := 0
-	i := 0
-	j := 0
-loop:
-	for i < len(*instructions) && i >= 0 && j < len(*instructions) && j >= 0 {
-		for (*instructions)[i].action == "rcv" && len(snd1) == 0 {
-			if (*instructions)[j].action == "rcv" && len(snd0) == 0 {
-				break loop
-			}
-			j, count = program(1, j, &snd0, &snd1, instructions, &registers1, count)
-		}
-		for (*instructions)[j].action == "rcv" && len(snd0) == 0 {
-			if (*instructions)[i].action == "rcv" && len(snd1) == 0 {
-				break loop
-			}
-			i, count = program(0, i, &snd0, &snd1, instructions, &registers0, count)
-		}
-		i, count = program(0, i, &snd0, &snd1, instructions, &registers0, count)
-		j, count = program(1, j, &snd0, &snd1, instructions, &registers1, count)
-	}
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go program(0, snd1, snd0, instructions, &count, &wg)
+	go program(1, snd0, snd1, instructions, &count, &wg)
+	wg.Wait()
 	return count
 }
