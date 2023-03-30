4c8ddafd0e4b4d78f6b8c198dd48724aa9e6465c
arkteck <arkteck@gmail.com>
2023-03-30 10:55:31 -0400

    Day 2017/24 part 2 solved

--- 2017/24/advent201724.go
+++ 2017/24/advent201724.go
@@ -15,6 +15,7 @@ func main() {
 	input := strings.Split(string(data), "\n")
 	components := parseInput(input)
 	fmt.Println(part1(components))
+	fmt.Println(part2(components))
 }
 
 type Component struct {
@@ -22,7 +23,7 @@ type Component struct {
 	strength int
 }
 
-func parseInput(input []string) map[int][]*Component {
+func parseInput(input []string) *map[int][]*Component {
 	components := map[int][]*Component{}
 	for _, line := range input {
 		nums := strings.Split(line, "/")
@@ -35,11 +36,11 @@ func parseInput(input []string) map[int][]*Component {
 		components[port1] = append(components[port1], &comp)
 		components[port2] = append(components[port2], &comp)
 	}
-	return components
+	return &components
 }
 
-func bridge(currentPort int, components map[int][]*Component, usedComponents map[*Component]bool, maxStrength *int, strength int) {
-	for _, component := range components[currentPort] {
+func bridge(currentPort int, components *map[int][]*Component, usedComponents map[*Component]bool, maxStrength *int, strength int) {
+	for _, component := range (*components)[currentPort] {
 		if !usedComponents[component] {
 			usedComponents[component] = true
 			port := component.ports[0]
@@ -55,9 +56,37 @@ func bridge(currentPort int, components map[int][]*Component, usedComponents map
 	}
 }
 
-func part1(components map[int][]*Component) int {
+func part1(components *map[int][]*Component) int {
 	maxStrength := 0
 	usedComponents := map[*Component]bool{}
 	bridge(0, components, usedComponents, &maxStrength, 0)
+	return maxStrength
+}
+
+func longBridge(currentPort int, components *map[int][]*Component, usedComponents map[*Component]bool, maxStrength *int, strength int, maxLength *int, length int) {
+	for _, component := range (*components)[currentPort] {
+		if !usedComponents[component] {
+			usedComponents[component] = true
+			port := component.ports[0]
+			if port == currentPort {
+				port = component.ports[1]
+			}
+			longBridge(port, components, usedComponents, maxStrength, strength+component.strength, maxLength, length+1)
+			usedComponents[component] = false
+		}
+	}
+	if length > *maxLength {
+		*maxLength = length
+		*maxStrength = strength
+	} else if length == *maxLength && strength > *maxStrength {
+		*maxStrength = strength
+	}
+}
+
+func part2(components *map[int][]*Component) int {
+	maxStrength := 0
+	maxLength := 0
+	usedComponents := map[*Component]bool{}
+	longBridge(0, components, usedComponents, &maxStrength, 0, &maxLength, 0)
 	return maxStrength
 }
