aac58aa79d1ed794772526c4cff87b81ce3782e3
arkteck <arkteck@gmail.com>
2023-03-31 11:23:34 -0400

    Day 2018/11 part 2 solved (super brute force, takes about a minute)

--- 2018/11/advent201811.go
+++ 2018/11/advent201811.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -11,22 +12,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	input, _ := strconv.Atoi(string(data))
-	fmt.Println(part1(input))
+	cells := parseInput(string(data))
+	fmt.Println(part1(cells))
+	fmt.Println(part2(cells))
 }
 
-func part1(input int) string {
+func parseInput(input string) *[300][300]int {
+	serial, _ := strconv.Atoi(input)
 	cells := [300][300]int{}
 	for i := 1; i <= 300; i++ {
 		for j := 1; j <= 300; j++ {
 			rackID := i + 10
-			power := rackID*j + input
+			power := rackID*j + serial
 			power *= rackID
 			power /= 100
 			power %= 10
 			cells[j-1][i-1] = power - 5
 		}
 	}
+	return &cells
+}
+
+func part1(cells *[300][300]int) string {
 	maxPower := 0
 	pos := [2]int{}
 	for i := 0; i < 297; i++ {
@@ -34,7 +41,7 @@ func part1(input int) string {
 			power := 0
 			for m := 0; m < 3; m++ {
 				for n := 0; n < 3; n++ {
-					power += cells[j+n][i+m]
+					power += (*cells)[j+n][i+m]
 				}
 			}
 			if power > maxPower {
@@ -45,4 +52,37 @@ func part1(input int) string {
 		}
 	}
 	return fmt.Sprintf("%d,%d", pos[0], pos[1])
+}
+
+func track(msg string) (string, time.Time) {
+	return msg, time.Now()
+}
+
+func duration(msg string, start time.Time) {
+	fmt.Printf("%v: %v\n", msg, time.Since(start))
+}
+
+func part2(cells *[300][300]int) string {
+	defer duration(track("part2"))
+	maxPower := 0
+	dims := [3]int{}
+	for i := 0; i < 300; i++ {
+		for j := 0; j < 300; j++ {
+			for k := 1; k+i <= 300 && k+j <= 300; k++ {
+				power := 0
+				for m := 0; m < k; m++ {
+					for n := 0; n < k; n++ {
+						power += (*cells)[j+n][i+m]
+					}
+				}
+				if power > maxPower {
+					maxPower = power
+					dims[0] = i + 1
+					dims[1] = j + 1
+					dims[2] = k
+				}
+			}
+		}
+	}
+	return fmt.Sprintf("%d,%d,%d", dims[0], dims[1], dims[2])
 }
