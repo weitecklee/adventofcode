9bd32df9cd887d0ad0afec631e497c8f29d931b2
arkteck <arkteck@gmail.com>
2023-04-01 10:31:27 -0400

    Day 2018/12 part 2 solved

--- 2018/12/advent201812.go
+++ 2018/12/advent201812.go
@@ -14,6 +14,7 @@ func main() {
 	input := strings.Split(string(data), "\n")
 	rules, startState, potLen := parseInput(input)
 	fmt.Println(part1(rules, startState, potLen))
+	fmt.Println(part2(rules, startState, potLen))
 }
 
 func parseInput(input []string) (map[string]string, map[int]string, int) {
@@ -59,4 +60,53 @@ func part1(rules map[string]string, pots map[int]string, potLen int) int {
 		}
 	}
 	return count
+}
+
+func part2(rules map[string]string, pots map[int]string, potLen int) int {
+	/*
+		Analysis of the growth pattern of `count` reveals that it reaches constant growth
+		after a certain number of generations. (For my input, it keeps growing by 8 after
+		153 generations)
+		Iterate until it reaches this constant growth and calculate the remaining growth
+		necessary (Just comparing the current difference and the previous difference is
+		enough, for mine at least)
+	*/
+	potRange := [2]int{0, potLen - 1}
+	count := 0
+	for i, s := range pots {
+		if s == "#" {
+			count += i
+		}
+	}
+	diff := 0
+	for generations := 1; generations < 1000; generations++ {
+		pots2 := map[int]string{}
+		potRange[0]--
+		potRange[1]++
+		for i := potRange[0]; i <= potRange[1]; i++ {
+			potState := ""
+			for j := -2; j <= 2; j++ {
+				if pots[i+j] == "" || pots[i+j] == "." {
+					potState += "."
+				} else {
+					potState += "#"
+				}
+			}
+			pots2[i] = rules[potState]
+		}
+		pots = pots2
+		count2 := 0
+		for i, s := range pots {
+			if s == "#" {
+				count2 += i
+			}
+		}
+		diff2 := count2 - count
+		if diff2 == diff {
+			return (50000000000-generations)*diff + count2
+		}
+		diff = diff2
+		count = count2
+	}
+	return -1
 }
