6dcd361c8df0bfd651073f23cfacde431a752910
arkteck <arkteck@gmail.com>
2023-04-06 10:44:10 -0400

    Day 2022/21 part 2 solved

--- 2022/21/advent202221.go
+++ 2022/21/advent202221.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -14,17 +15,18 @@ func main() {
 	}
 	input := strings.Split(string(data), "\n")
 	fmt.Println(part1(parseInput(input)))
+	fmt.Println(part2(parseInput(input)))
 }
 
 type Monkey struct {
-	value     int
+	value     float64
 	monkey1   *Monkey
 	monkey2   *Monkey
 	operation string
 }
 
-func (m *Monkey) Value() int {
-	if m.value > 0 {
+func (m *Monkey) Value() float64 {
+	if m.value > 0 { // assume no monkey has value zero
 		return m.value
 	}
 	switch m.operation {
@@ -61,18 +63,58 @@ func parseInput(input []string) *map[string]*Monkey {
 			monkeys[curr].operation = parts[2]
 		} else {
 			num, _ := strconv.Atoi(parts[1])
-			monkeys[curr].value = num
-		}
-	}
-	for _, monkey := range monkeys {
-		// assume no monkey has value of zero
-		if monkey.value == 0 {
-			monkey.value = monkey.Value()
+			monkeys[curr].value = float64(num)
 		}
 	}
 	return &monkeys
 }
 
 func part1(monkeys *map[string]*Monkey) int {
-	return (*monkeys)["root"].value
+	return int((*monkeys)["root"].Value())
+}
+
+func part2(monkeys *map[string]*Monkey) int {
+	// find which side "humn" is on
+	monkeyh := (*monkeys)["root"].monkey1
+	monkey2 := (*monkeys)["root"].monkey2
+	monkeyhvalue := monkeyh.Value()
+	monkey2value := monkey2.Value()
+	(*monkeys)["humn"].value = (*monkeys)["humn"].value * 2
+	correlation := 1 // flag for positive/negative correlation
+	monkeyhvalue2 := monkeyh.Value()
+	if monkeyhvalue == monkeyhvalue2 {
+		if monkey2.Value() < monkey2value {
+			correlation = -1
+		}
+		monkeyh = monkey2
+		monkey2 = (*monkeys)["root"].monkey1
+	} else if monkeyhvalue2 < monkeyhvalue {
+		correlation = -1
+	}
+	valueToMatch := monkey2.Value()
+	lo := 1.0
+	hi := math.MaxFloat64
+	for lo < hi {
+		mi := math.Floor(lo + (hi-lo)/2)
+		(*monkeys)["humn"].value = mi
+		monkeyhvalue = monkeyh.Value()
+		if monkeyhvalue == valueToMatch {
+			lo = mi
+			break
+		}
+		if monkeyhvalue < valueToMatch {
+			if correlation > 0 {
+				lo = mi + 1
+			} else {
+				hi = mi - 1
+			}
+		} else {
+			if correlation > 0 {
+				hi = mi - 1
+			} else {
+				lo = mi + 1
+			}
+		}
+	}
+	return int(lo)
 }
