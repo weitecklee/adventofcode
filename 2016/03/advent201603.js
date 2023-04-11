c50fd96d07bec932bf1a88b970a4a04c63aefb85
arkteck <arkteck@gmail.com>
2023-04-11 10:45:04 -0400

    Day 2016/03 solved

---
+++ 2016/03/advent201603.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err)
+  } else {
+    return data;
+  }
+}).split('\n');
+
+let count = 0;
+const parsedInput = [];
+
+const checkTriangle = (arr) => (arr[0] + arr[1] > arr[2] && arr[0] + arr[2] > arr[1] && arr[1] + arr[2] > arr[0]);
+
+for (const line of input) {
+  const matches = line.match(/\d+/g);
+  const sides = [];
+  for (const match of matches) {
+    sides.push(Number(match));
+  }
+  parsedInput.push(sides);
+  if (checkTriangle(sides)) {
+    count++;
+  }
+}
+
+console.log(count);
+
+let count2 = 0;
+
+for (let i = 0; i < parsedInput.length; i += 3) {
+  for (let j = 0; j < 3; j++) {
+    const sides = [parsedInput[i][j], parsedInput[i + 1][j], parsedInput[i + 2][j]];
+    if (checkTriangle(sides)) {
+      count2++;
+    }
+  }
+}
+
+console.log(count2);
