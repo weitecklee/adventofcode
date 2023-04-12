3907fafb9042c53188a943ef33196a07cc0683a5
arkteck <arkteck@gmail.com>
2023-04-12 15:21:32 -0400

    Day 2016/09 solved

---
+++ 2016/09/advent201609.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err)
+  } else {
+    return data;
+  }
+});
+
+const decompress = (code) => {
+  let i = 0;
+  let res = 0;
+  while (i < code.length) {
+    while (i < code.length && code[i] !== '(') {
+      res++;
+      i++;
+    }
+    if (i >= code.length) {
+      break;
+    }
+    let j = i;
+    while (code[j] !== ')') {
+      j++;
+    }
+    const marker = code.slice(i + 1, j);
+    const nums = marker.split('x').map(Number);
+    res += nums[0] * nums[1];
+    i = j + 1 + nums[0];
+  }
+  return res;
+}
+
+const decompress2 = (code) => {
+  let i = 0;
+  let res = 0;
+  while (i < code.length) {
+    while (i < code.length && code[i] !== '(') {
+      res++;
+      i++;
+    }
+    if (i >= code.length) {
+      break;
+    }
+    let j = i;
+    let balance = 1;
+    while (code[j] !== ')') {
+      j++;
+    }
+    const marker = code.slice(i + 1, j);
+    const nums = marker.split('x').map(Number);
+    res += decompress2(code.slice(j + 1, j + 1 + nums[0])) * nums[1];
+    i = j + 1 + nums[0];
+  }
+  return res;
+}
+
+console.log(decompress(input));
+console.log(decompress2(input));
