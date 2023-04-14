9d7232d4c638bc5b411f25b8c77aed34720f5340
arkteck <arkteck@gmail.com>
2023-04-14 10:45:47 -0400

    Day 2016/13 part 2 solved

--- 2016/13/advent201613.go
+++ 2016/13/advent201613.go
@@ -13,6 +13,7 @@ func main() {
 	}
 	input, _ := strconv.Atoi(string(data))
 	fmt.Println(part1(input))
+	fmt.Println(part2(input))
 }
 
 func mapout(coord [2]int, layout *map[[2]int]bool, input int) bool {
@@ -63,4 +64,36 @@ func part1(input int) int {
 		i++
 	}
 	return -1
+}
+
+func part2(input int) int {
+	queue := [][3]int{{1, 1, 0}}
+	steps := [][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+	layout := map[[2]int]bool{}
+	visited := map[[2]int]int{}
+	layout[[2]int{1, 1}] = true
+	visited[[2]int{1, 1}] = 0
+	i := 0
+	for i < len(queue) {
+		current := queue[i]
+		if current[2] < 50 {
+			pos := [2]int{current[0], current[1]}
+			dist := current[2]
+			for _, step := range steps {
+				coord := [2]int{pos[0] + step[0], pos[1] + step[1]}
+				if coord[0] < 0 || coord[1] < 0 {
+					continue
+				}
+				if mapout(coord, &layout, input) {
+					d, ok := visited[coord]
+					if !ok || dist+1 < d {
+						queue = append(queue, [3]int{coord[0], coord[1], dist + 1})
+						visited[coord] = dist + 1
+					}
+				}
+			}
+		}
+		i++
+	}
+	return len(visited)
 }
