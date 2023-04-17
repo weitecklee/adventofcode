3b02081aa2585d4888b986c8a41c6acbf861770d
arkteck <arkteck@gmail.com>
2023-04-17 10:31:36 -0400

    Day 2016/16 solved

---
+++ 2016/16/advent201616.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(data)
+	fmt.Println(part1(input))
+	fmt.Println(part2(input))
+}
+
+func dragon(a string) string {
+	var b strings.Builder
+	for i := len(a) - 1; i >= 0; i-- {
+		if a[i] == '0' {
+			b.WriteString("1")
+		} else {
+			b.WriteString("0")
+		}
+	}
+	return a + "0" + b.String()
+}
+
+func checksum(s string, l int) string {
+	for len(s) < l {
+		s = dragon(s)
+	}
+	s = s[:l]
+	for len(s)%2 == 0 {
+		var check strings.Builder
+		for i := 0; i < len(s); i += 2 {
+			if s[i] == s[i+1] {
+				check.WriteString("1")
+			} else {
+				check.WriteString("0")
+			}
+		}
+		s = check.String()
+	}
+	return s
+}
+
+func part1(s string) string {
+	return checksum(s, 272)
+}
+
+func part2(s string) string {
+	return checksum(s, 35651584)
+}
