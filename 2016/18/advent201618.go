bd47c8c764bf82f80a8e652968cacf458de10ba2
arkteck <arkteck@gmail.com>
2023-04-19 10:17:34 -0400

    Day 2016/18 solved

---
+++ 2016/18/advent201618.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(data)
+	fmt.Println(solve(input))
+}
+
+func trapCheck(tiles string) string {
+	switch tiles {
+	case "^^.":
+		return "^"
+	case ".^^":
+		return "^"
+	case "^..":
+		return "^"
+	case "..^":
+		return "^"
+	default:
+		return "."
+	}
+}
+
+func safeCheck(tiles string) int {
+	n := 0
+	for _, c := range tiles {
+		if c == '.' {
+			n++
+		}
+	}
+	return n
+}
+
+func solve(row string) (int, int) {
+	safeTiles := safeCheck(row)
+	for i := 1; i < 40; i++ {
+		row2 := ""
+		rowCheck := "." + row + "."
+		for j := 0; j < len(rowCheck)-2; j++ {
+			row2 += trapCheck(rowCheck[j : j+3])
+		}
+		row = row2
+		safeTiles += safeCheck(row)
+	}
+	safeTiles2 := safeTiles
+	for i := 40; i < 400000; i++ {
+		row2 := ""
+		rowCheck := "." + row + "."
+		for j := 0; j < len(rowCheck)-2; j++ {
+			row2 += trapCheck(rowCheck[j : j+3])
+		}
+		row = row2
+		safeTiles2 += safeCheck(row)
+	}
+	return safeTiles, safeTiles2
+}
