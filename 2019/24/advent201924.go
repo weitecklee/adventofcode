cb90a005ba521a41ce43dd46eb5cbc990585fb3c
arkteck <arkteck@gmail.com>
2023-04-27 10:59:06 -0400

    Day 2019/24 part 1 solved

---
+++ 2019/24/advent201924.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := strings.Split(string(data), "\n")
+	fmt.Println(part1(parseInput(input)))
+}
+
+func parseInput(input []string) map[[2]int]bool {
+	bugs := map[[2]int]bool{}
+	for j, row := range input {
+		for i, c := range row {
+			if c == '#' {
+				bugs[[2]int{i, j}] = true
+			}
+		}
+	}
+	return bugs
+}
+
+func state(bugs map[[2]int]bool) int {
+	stateNum := 0.0
+	for coord := range bugs {
+		stateNum += math.Pow(2, float64(5*coord[1]+coord[0]))
+	}
+	return int(stateNum)
+}
+
+func aBugsLife(bugs map[[2]int]bool) map[[2]int]bool {
+	bugs2 := map[[2]int]bool{}
+	adjacents := map[[2]int]int{}
+	checks := [][2]int{
+		{-1, 0},
+		{1, 0},
+		{0, -1},
+		{0, 1},
+	}
+	for coord := range bugs {
+		for _, check := range checks {
+			toCheck := [2]int{coord[0] + check[0], coord[1] + check[1]}
+			if toCheck[0] < 0 || toCheck[1] < 0 || toCheck[0] > 4 || toCheck[1] > 4 {
+				continue
+			}
+			adjacents[toCheck]++
+		}
+	}
+	for coord := range bugs {
+		if adjacents[coord] == 1 {
+			bugs2[coord] = true
+		}
+	}
+	for coord, n := range adjacents {
+		if !bugs[coord] && (n == 1 || n == 2) {
+			bugs2[coord] = true
+		}
+	}
+	return bugs2
+}
+
+func part1(bugs map[[2]int]bool) int {
+	stateHistory := map[int]bool{}
+	stateHistory[state(bugs)] = true
+	for {
+		bugs = aBugsLife(bugs)
+		currState := state(bugs)
+		if stateHistory[currState] {
+			return currState
+		}
+		stateHistory[currState] = true
+	}
+	return -1
+}
