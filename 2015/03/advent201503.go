669897da3d5575f327423d06f3194dba078c97aa
arkteck <arkteck@gmail.com>
2023-05-01 10:09:18 -0400

    Day 2015/03 solved

---
+++ 2015/03/advent201503.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(data)
+	fmt.Println(part1(input))
+	fmt.Println(part2(input))
+}
+
+func part1(input string) int {
+	houses := map[[2]int]bool{}
+	loc := [2]int{0, 0}
+	houses[loc] = true
+	for _, dir := range input {
+		switch dir {
+		case '^':
+			loc[1]++
+		case 'v':
+			loc[1]--
+		case '<':
+			loc[0]--
+		case '>':
+			loc[0]++
+		}
+		houses[loc] = true
+	}
+	return len(houses)
+}
+
+func part2(input string) int {
+	houses := map[[2]int]bool{}
+	locs := [2][2]int{
+		{0, 0},
+		{0, 0},
+	}
+	houses[locs[0]] = true
+	for i, dir := range input {
+		switch dir {
+		case '^':
+			locs[i%2][1]++
+		case 'v':
+			locs[i%2][1]--
+		case '<':
+			locs[i%2][0]--
+		case '>':
+			locs[i%2][0]++
+		}
+		houses[locs[i%2]] = true
+	}
+	return len(houses)
+}
