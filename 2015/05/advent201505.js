ad30dcf9556c5f0079e445d268a0f4cfdf0d6a12
arkteck <arkteck@gmail.com>
2023-05-01 10:38:47 -0400

    Day 2015/05 solved

---
+++ 2015/05/advent201505.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err)
+  } else {
+    return data;
+  }
+}).split('\n');
+
+const re1 = /[aeiou]/g;
+const re2 = /(\w)\1/;
+const re3 = /ab|cd|pq|xy/;
+
+let count = 0;
+for (const line of input) {
+  if (re1.test(line) && line.match(re1).length >= 3 && re2.test(line) && !re3.test(line)) {
+    count++;
+  }
+}
+console.log(count);
+
+const re4 = /(\w\w).*\1/;
+const re5 = /(\w)\w\1/;
+
+let count2 = 0;
+for (const line of input) {
+  if (re4.test(line) && re5.test(line)) {
+    count2++;
+  }
+}
+console.log(count2);
