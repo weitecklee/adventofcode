a9b7b550511422d0e769ed4acd1f65b1ad8d4c69
arkteck <arkteck@gmail.com>
2023-05-01 10:40:51 -0400

    Refactor to one loop

--- 2015/05/advent201505.js
+++ 2015/05/advent201505.js
@@ -11,22 +11,20 @@ const input = fs.readFileSync('input.txt', 'utf-8', (err, data) => {
 const re1 = /[aeiou]/g;
 const re2 = /(\w)\1/;
 const re3 = /ab|cd|pq|xy/;
+const re4 = /(\w\w).*\1/;
+const re5 = /(\w)\w\1/;
 
 let count = 0;
+let count2 = 0;
+
 for (const line of input) {
   if (re1.test(line) && line.match(re1).length >= 3 && re2.test(line) && !re3.test(line)) {
     count++;
   }
-}
-console.log(count);
-
-const re4 = /(\w\w).*\1/;
-const re5 = /(\w)\w\1/;
-
-let count2 = 0;
-for (const line of input) {
   if (re4.test(line) && re5.test(line)) {
     count2++;
   }
 }
+
+console.log(count);
 console.log(count2);
