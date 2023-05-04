4b166789b13e7138493ac659fc6bd1662c1c4a88
arkteck <arkteck@gmail.com>
2023-05-04 11:58:06 -0400

    Day 2015/12 part 2 solved

--- 2015/12/advent201512.go
+++ 2015/12/advent201512.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"regexp"
@@ -13,6 +14,7 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(part1(string(data)))
+	fmt.Println(part2(data))
 }
 
 func part1(input string) int {
@@ -24,4 +26,40 @@ func part1(input string) int {
 		res += n
 	}
 	return res
+}
+
+func recurMap(thing map[string]any) float64 {
+	sum := 0.0
+	for _, v := range thing {
+		if v == "red" {
+			return 0
+		} else if m, ok := v.(map[string]any); ok {
+			sum += recurMap(m)
+		} else if a, ok := v.([]any); ok {
+			sum += recurArr(a)
+		} else if n, ok := v.(float64); ok {
+			sum += n
+		}
+	}
+	return sum
+}
+
+func recurArr(thing []any) float64 {
+	sum := 0.0
+	for _, v := range thing {
+		if m, ok := v.(map[string]any); ok {
+			sum += recurMap(m)
+		} else if a, ok := v.([]any); ok {
+			sum += recurArr(a)
+		} else if n, ok := v.(float64); ok {
+			sum += n
+		}
+	}
+	return sum
+}
+
+func part2(data []byte) float64 {
+	var store map[string]any
+	json.Unmarshal(data, &store)
+	return recurMap(store)
 }
