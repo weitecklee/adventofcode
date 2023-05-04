0f20968ff9b445cda67c5c1df4d3ffed69ad0745
arkteck <arkteck@gmail.com>
2023-05-04 11:09:57 -0400

    Refactor to separate parts, day 2015/11 part 2 solved

--- 2015/11/advent201511.go
+++ 2015/11/advent201511.go
@@ -11,46 +11,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(part1(string(data)))
+	part1 := nextPassword(string(data))
+	part2 := nextPassword(part1)
+	fmt.Println(part1, part2)
 }
 
-func part1(password string) string {
+func nextPassword(password string) string {
 	pw := []rune(password)
-	valid := false
-	for !valid {
-		i := len(pw) - 1
-		for i >= 0 {
-			pw[i]++
-			if pw[i] == 105 || pw[i] == 108 || pw[i] == 111 {
-				pw[i]++
-			}
-			if pw[i] > 122 {
-				pw[i] = 97
-				i--
-			} else {
-				break
-			}
-		}
-		for j := 0; j < len(pw)-2; j++ {
-			if pw[j] == pw[j+1]-1 && pw[j+1] == pw[j+2]-1 {
-				valid = true
-				break
-			}
-		}
-		if !valid {
-			continue
-		}
-		valid = false
-		for j := 0; j < len(pw)-1; j++ {
-			if pw[j] == pw[j+1] {
-				for k := j + 2; k < len(pw)-1; k++ {
-					if pw[k] == pw[k+1] && pw[j] != pw[k] {
-						valid = true
-						break
-					}
-				}
-				break
-			}
+	for {
+		incrementPassword(&pw)
+		if validatePassword(&pw) {
+			break
 		}
 	}
 	var nextPassword strings.Builder
@@ -58,4 +29,46 @@ func part1(password string) string {
 		nextPassword.WriteRune(r)
 	}
 	return nextPassword.String()
+}
+
+func incrementPassword(pw *[]rune) {
+	i := len(*pw) - 1
+	for i >= 0 {
+		(*pw)[i]++
+		if (*pw)[i] == 105 || (*pw)[i] == 108 || (*pw)[i] == 111 {
+			(*pw)[i]++
+		}
+		if (*pw)[i] > 122 {
+			(*pw)[i] = 97
+			i--
+		} else {
+			break
+		}
+	}
+}
+
+func validatePassword(pw *[]rune) bool {
+	valid := false
+	for j := 0; j < len(*pw)-2; j++ {
+		if (*pw)[j] == (*pw)[j+1]-1 && (*pw)[j+1] == (*pw)[j+2]-1 {
+			valid = true
+			break
+		}
+	}
+	if !valid {
+		return false
+	}
+	valid = false
+	for j := 0; j < len(*pw)-1; j++ {
+		if (*pw)[j] == (*pw)[j+1] {
+			for k := j + 2; k < len((*pw))-1; k++ {
+				if (*pw)[k] == (*pw)[k+1] && (*pw)[j] != (*pw)[k] {
+					valid = true
+					break
+				}
+			}
+			break
+		}
+	}
+	return valid
 }
