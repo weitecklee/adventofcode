59880eae8226d5af068648a63ac4dd51d6c97ac4
arkteck <arkteck@gmail.com>
2023-05-09 11:02:50 -0400

    Refactor to reuse code

--- 2015/18/advent201518.go
+++ 2015/18/advent201518.go
@@ -30,39 +30,19 @@ func parseInput(input []string) map[[2]int]bool {
 }
 
 func part1(lights map[[2]int]bool) int {
-	for i := 0; i < 100; i++ {
-		neighbors := map[[2]int]int{}
-		for coord := range lights {
-			for j := -1; j <= 1; j++ {
-				for k := -1; k <= 1; k++ {
-					if (j == 0 && k == 0) || coord[0]+j < 0 || coord[0]+j >= 100 || coord[1]+k < 0 || coord[1]+k >= 100 {
-						continue
-					}
-					coord2 := [2]int{coord[0] + j, coord[1] + k}
-					neighbors[coord2]++
-				}
-			}
-		}
-		lights2 := map[[2]int]bool{}
-		for coord := range lights {
-			if neighbors[coord] == 2 || neighbors[coord] == 3 {
-				lights2[coord] = true
-			}
-		}
-		for coord, n := range neighbors {
-			if n == 3 && !lights[coord] {
-				lights2[coord] = true
-			}
-		}
-		lights = lights2
-	}
-	return len(lights)
+	return gameOfLife(lights, false)
 }
 
 func part2(lights map[[2]int]bool) int {
+	return gameOfLife(lights, true)
+}
+
+func gameOfLife(lights map[[2]int]bool, part2 bool) int {
 	corners := [][2]int{{0, 0}, {0, 99}, {99, 0}, {99, 99}}
-	for _, coord := range corners {
-		lights[coord] = true
+	if part2 {
+		for _, coord := range corners {
+			lights[coord] = true
+		}
 	}
 	for i := 0; i < 100; i++ {
 		neighbors := map[[2]int]int{}
@@ -88,8 +68,10 @@ func part2(lights map[[2]int]bool) int {
 				lights2[coord] = true
 			}
 		}
-		for _, coord := range corners {
-			lights2[coord] = true
+		if part2 {
+			for _, coord := range corners {
+				lights2[coord] = true
+			}
 		}
 		lights = lights2
 	}
