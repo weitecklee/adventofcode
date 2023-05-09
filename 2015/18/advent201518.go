c3cdd92638786e7033d8c428e560dd94207c1db7
arkteck <arkteck@gmail.com>
2023-05-09 10:58:38 -0400

    Day 2015/18 part 2 solved

--- 2015/18/advent201518.go
+++ 2015/18/advent201518.go
@@ -14,6 +14,7 @@ func main() {
 	input := strings.Split(string(data), "\n")
 	lights := parseInput(input)
 	fmt.Println(part1(lights))
+	fmt.Println(part2(lights))
 }
 
 func parseInput(input []string) map[[2]int]bool {
@@ -52,6 +53,43 @@ func part1(lights map[[2]int]bool) int {
 			if n == 3 && !lights[coord] {
 				lights2[coord] = true
 			}
+		}
+		lights = lights2
+	}
+	return len(lights)
+}
+
+func part2(lights map[[2]int]bool) int {
+	corners := [][2]int{{0, 0}, {0, 99}, {99, 0}, {99, 99}}
+	for _, coord := range corners {
+		lights[coord] = true
+	}
+	for i := 0; i < 100; i++ {
+		neighbors := map[[2]int]int{}
+		for coord := range lights {
+			for j := -1; j <= 1; j++ {
+				for k := -1; k <= 1; k++ {
+					if (j == 0 && k == 0) || coord[0]+j < 0 || coord[0]+j >= 100 || coord[1]+k < 0 || coord[1]+k >= 100 {
+						continue
+					}
+					coord2 := [2]int{coord[0] + j, coord[1] + k}
+					neighbors[coord2]++
+				}
+			}
+		}
+		lights2 := map[[2]int]bool{}
+		for coord := range lights {
+			if neighbors[coord] == 2 || neighbors[coord] == 3 {
+				lights2[coord] = true
+			}
+		}
+		for coord, n := range neighbors {
+			if n == 3 && !lights[coord] {
+				lights2[coord] = true
+			}
+		}
+		for _, coord := range corners {
+			lights2[coord] = true
 		}
 		lights = lights2
 	}
