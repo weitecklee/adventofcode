3ef2dc5698dc599b22845685761820a9bba384ba
arkteck <arkteck@gmail.com>
2023-05-10 11:59:37 -0400

    Day 2015/19 part 2 solution taken from reddit

--- 2015/19/advent201519.go
+++ 2015/19/advent201519.go
@@ -15,9 +15,10 @@ func main() {
 	input := strings.Split(string(data), "\n")
 	replacements, molecule := parseInput(input)
 	fmt.Println(part1(replacements, molecule))
+	fmt.Println(part2(molecule))
 }
 
-func parseInput(input []string) (map[string][]string, string) {
+func parseInput(input []string) (*map[string][]string, string) {
 	replacements := map[string][]string{}
 	for i := 0; i < len(input)-2; i++ {
 		parts := strings.Split(input[i], " => ")
@@ -26,12 +27,12 @@ func parseInput(input []string) (map[string][]string, string) {
 		}
 		replacements[parts[0]] = append(replacements[parts[0]], parts[1])
 	}
-	return replacements, input[len(input)-1]
+	return &replacements, input[len(input)-1]
 }
 
-func part1(replacements map[string][]string, molecule string) int {
+func replaceMolecules(replacements *map[string][]string, molecule string) *[]string {
 	res := map[string]bool{}
-	for orig, reps := range replacements {
+	for orig, reps := range *replacements {
 		re := regexp.MustCompile(orig)
 		indices := re.FindAllStringIndex(molecule, -1)
 		if indices != nil {
@@ -43,5 +44,35 @@ func part1(replacements map[string][]string, molecule string) int {
 			}
 		}
 	}
-	return len(res)
+	molecules := make([]string, 0, len(res))
+	for molec := range res {
+		molecules = append(molecules, molec)
+	}
+	return &molecules
+}
+
+func part1(replacements *map[string][]string, molecule string) int {
+	return len(*replaceMolecules(replacements, molecule))
+}
+
+/*
+	First, tried BFS using refactored replaceMolecules function.
+	Tree blows up way too big to be feasible.
+	Then, tried the reverse by going from the target back to "e".
+	Again, tree blows up way too big to be feasible.
+	Reading others' solutions on reddit, they used greedy algo that always
+	uses the longest replacement available. This is not a general solution.
+	Indeed, it doesn't work for my input.
+
+	Finally, used the solution provided by u/askalski, comment copied in solution.txt for posterity.
+	https://www.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/cy4etju/
+*/
+
+func part2(molecule string) int {
+	re := regexp.MustCompile(`[A-Z][a-z]?`)
+	countMolecules := len(re.FindAllString(molecule, -1))
+	countRn := strings.Count(molecule, "Rn")
+	countAr := strings.Count(molecule, "Ar")
+	countY := strings.Count(molecule, "Y")
+	return countMolecules - countRn - countAr - 2*countY - 1
 }
