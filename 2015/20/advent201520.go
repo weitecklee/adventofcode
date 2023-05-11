11eb21a720e5ac46f3114e1aaa0e5f443699372a
arkteck <arkteck@gmail.com>
2023-05-11 12:19:41 -0400

    Day 2015/20 part 2 solved

--- 2015/20/advent201520.go
+++ 2015/20/advent201520.go
@@ -14,19 +14,20 @@ func main() {
 	}
 	input, _ := strconv.Atoi(string(data))
 	fmt.Println(part1(input))
+	fmt.Println(part2(input))
 }
 
 func presents(n int) int {
 	sum := 1 + n
 	k := int(math.Sqrt(float64(n)))
-	for i := 2; i <= k; i++ {
+	for i := 2; i < k; i++ {
 		if n%i == 0 {
 			sum += i
 			sum += n / i
 		}
 	}
 	if k*k == n {
-		sum -= k
+		sum += k
 	}
 	return sum * 10
 }
@@ -37,6 +38,35 @@ func part1(input int) int {
 	for n < input {
 		i++
 		n = presents(i)
+	}
+	return i
+}
+
+func presents2(n int) int {
+	sum := 0
+	k := int(math.Sqrt(float64(n)))
+	for i := 1; i < k; i++ {
+		if n%i == 0 {
+			if 50*i >= n {
+				sum += i
+			}
+			if 50*n/i >= n {
+				sum += n / i
+			}
+		}
+	}
+	if k*k == n && 50*k >= n {
+		sum += k
+	}
+	return sum * 11
+}
+
+func part2(input int) int {
+	i := 1
+	n := presents2(i)
+	for n < input {
+		i++
+		n = presents2(i)
 	}
 	return i
 }
