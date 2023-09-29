6c9e9d3c311ae754ce5ac3f54dabf7bcfdf6dd66
arkteck <arkteck@gmail.com>
2023-09-29 09:55:26 -0400

    Day 2021/06 in python

---
+++ 2021/06/advent202106.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def simulate(puzzle_input: List[int], days: int) -> int:
+  fish = [0] * 9
+  for i in puzzle_input:
+    fish[i] += 1
+  for _ in range(days):
+    fish2 = [0] * 9
+    for i in range(8):
+      fish2[i] = fish[i + 1]
+    fish2[6] += fish[0]
+    fish2[8] = fish[0]
+    fish = fish2
+  return sum(fish)
+
+if __name__ == "__main__":
+  with open('input.txt','r') as file:
+    puzzle_input = [int(n) for n in file.readline().split(',')]
+  print(simulate(puzzle_input, 80))
+  print(simulate(puzzle_input, 256))
