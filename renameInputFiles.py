5f598a8156b631d35aa9edf17bd44b775eeaa31f
arkteck <arkteck@gmail.com>
2024-01-04 10:03:20 -0500

    rename folders

--- renameInputFiles.py
+++
@@ -1,8 +0,0 @@
-import os, glob, re
-
-def main():
-  for file in glob.glob("**/input*", recursive = True):
-    os.rename(file, re.sub('input\d+', 'input', file))
-
-if __name__ == "__main__":
-  main()
