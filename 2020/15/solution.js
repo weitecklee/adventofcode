efa2597c7fd850864b83bf707393779b3747e4ee
arkteck <arkteck@gmail.com>
2024-11-18 14:49:26 -0500

    Day 2020/15 in JS

---
+++ 2020/15/solution.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
+  .split(",")
+  .map(Number);
+
+const gameHistory = new Map();
+
+input.forEach((num, i) => gameHistory.set(num, i + 1));
+
+let turn = input.length + 1;
+let currentNumber = 0;
+
+while (turn < 2020) {
+  let nextNumber = 0;
+  if (gameHistory.has(currentNumber)) {
+    nextNumber = turn - gameHistory.get(currentNumber);
+  }
+  gameHistory.set(currentNumber, turn);
+  currentNumber = nextNumber;
+  turn++;
+}
+
+console.log(currentNumber);
+
+while (turn < 30000000) {
+  let nextNumber = 0;
+  if (gameHistory.has(currentNumber)) {
+    nextNumber = turn - gameHistory.get(currentNumber);
+  }
+  gameHistory.set(currentNumber, turn);
+  currentNumber = nextNumber;
+  turn++;
+}
+
+console.log(currentNumber);
