e8c948442c4853c87d26a0d018e86c9b32d6db57
arkteck <arkteck@gmail.com>
2024-11-18 14:52:18 -0500

    Day 2020/15 in JS refactored

--- 2020/15/solution.js
+++ 2020/15/solution.js
@@ -6,33 +6,56 @@ const input = fs
   .split(",")
   .map(Number);
 
-const gameHistory = new Map();
+// const gameHistory = new Map();
 
-input.forEach((num, i) => gameHistory.set(num, i + 1));
+// input.forEach((num, i) => gameHistory.set(num, i + 1));
 
-let turn = input.length + 1;
-let currentNumber = 0;
+// let turn = input.length + 1;
+// let currentNumber = 0;
 
-while (turn < 2020) {
-  let nextNumber = 0;
-  if (gameHistory.has(currentNumber)) {
-    nextNumber = turn - gameHistory.get(currentNumber);
+// while (turn < 2020) {
+//   let nextNumber = 0;
+//   if (gameHistory.has(currentNumber)) {
+//     nextNumber = turn - gameHistory.get(currentNumber);
+//   }
+//   gameHistory.set(currentNumber, turn);
+//   currentNumber = nextNumber;
+//   turn++;
+// }
+
+// console.log(currentNumber);
+
+// while (turn < 30000000) {
+//   let nextNumber = 0;
+//   if (gameHistory.has(currentNumber)) {
+//     nextNumber = turn - gameHistory.get(currentNumber);
+//   }
+//   gameHistory.set(currentNumber, turn);
+//   currentNumber = nextNumber;
+//   turn++;
+// }
+
+// console.log(currentNumber);
+
+function playMemoryGame(input, turns) {
+  const gameHistory = new Map();
+
+  input.forEach((num, i) => gameHistory.set(num, i + 1));
+
+  let turn = input.length + 1;
+  let currentNumber = 0;
+
+  while (turn < turns) {
+    let nextNumber = 0;
+    if (gameHistory.has(currentNumber)) {
+      nextNumber = turn - gameHistory.get(currentNumber);
+    }
+    gameHistory.set(currentNumber, turn);
+    currentNumber = nextNumber;
+    turn++;
   }
-  gameHistory.set(currentNumber, turn);
-  currentNumber = nextNumber;
-  turn++;
+  return currentNumber;
 }
 
-console.log(currentNumber);
-
-while (turn < 30000000) {
-  let nextNumber = 0;
-  if (gameHistory.has(currentNumber)) {
-    nextNumber = turn - gameHistory.get(currentNumber);
-  }
-  gameHistory.set(currentNumber, turn);
-  currentNumber = nextNumber;
-  turn++;
-}
-
-console.log(currentNumber);
+console.log(playMemoryGame(input, 2020));
+console.log(playMemoryGame(input, 30000000));
