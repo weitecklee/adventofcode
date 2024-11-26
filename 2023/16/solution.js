0235dad6b1d5ca4bfeec8998190ee84da10510d9
arkteck <arkteck@gmail.com>
2024-11-26 11:41:59 -0500

    Day 2023/16 Part 1 in JS

---
+++ 2023/16/solution.js
@@ -0,0 +1,88 @@
+const fs = require("fs");
+const path = require("path");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
+  .split("\n");
+
+const energyHistory = new Map();
+
+class Beam {
+  constructor(pos, dir) {
+    this.pos = pos;
+    this.dir = dir;
+  }
+
+  advance() {
+    // returns [success, newBeam]
+    // if !success, discard beam (hit wall or already visited)
+    // if success, keep beam and if newBeam is not null, add it to beams
+    this.pos[0] += this.dir[0];
+    this.pos[1] += this.dir[1];
+    if (
+      this.pos[0] < 0 ||
+      this.pos[0] >= input[0].length ||
+      this.pos[1] < 0 ||
+      this.pos[1] >= input.length
+    ) {
+      return [false, null];
+    }
+    const beamPos = this.pos.join(",");
+    const beamDir = this.dir.join(",");
+    if (energyHistory.has(beamPos)) {
+      const history = energyHistory.get(beamPos);
+      if (history.has(beamDir)) {
+        return [false, null];
+      } else {
+        history.set(beamDir, true);
+      }
+    } else {
+      energyHistory.set(beamPos, new Map([[beamDir, true]]));
+    }
+    const c = input[this.pos[1]][this.pos[0]];
+    switch (c) {
+      case ".":
+        return [true, null];
+      case "|":
+        if (this.dir[0] === 0) {
+          return [true, null];
+        } else {
+          this.dir = [0, 1];
+          return [true, new Beam(this.pos.slice(), [0, -1])];
+        }
+      case "-":
+        if (this.dir[1] === 0) {
+          return [true, null];
+        } else {
+          this.dir = [1, 0];
+          return [true, new Beam(this.pos.slice(), [-1, 0])];
+        }
+      case "/":
+        this.dir = [-this.dir[1], -this.dir[0]];
+        return [true, null];
+      case "\\":
+        this.dir = [this.dir[1], this.dir[0]];
+        return [true, null];
+      default:
+        throw new Error("Invalid character: ", c);
+    }
+  }
+}
+
+let beams = [new Beam([-1, 0], [1, 0])];
+
+while (beams.length) {
+  const newBeams = [];
+  for (const beam of beams) {
+    const [success, newBeam] = beam.advance();
+    if (success) {
+      newBeams.push(beam);
+      if (newBeam) {
+        newBeams.push(newBeam);
+      }
+    }
+  }
+  beams = newBeams;
+}
+
+console.log(energyHistory.size);
