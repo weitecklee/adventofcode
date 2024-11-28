8adf52f534b1c51c2d576f82cd826e70b0e66056
arkteck <arkteck@gmail.com>
2024-11-27 19:54:28 -0500

    Day 2023/17 refactored with minHeap

--- 2023/17/solution.js
+++ 2023/17/solution.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
 const path = require("path");
+const MinHeap = require("../../utils/MinHeap");
 
 const input = fs
   .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
   .split("\n")
   .map((a) => a.split("").map(Number));
 
-const queue = [[0, 0, -1, 0, 0]]; // x, y, directionIndex, steps in current direction, heat loss
+const queue = [[0, 0, 0, -1, 0]]; // heat loss, x, y, directionIndex, steps in current direction
 const turnDirections = [
   [0, 1],
   [1, 0],
@@ -20,7 +21,7 @@ function stateKey(x, y, dir, steps) {
 }
 
 while (queue.length) {
-  const [x, y, dirIndex, steps, heatLoss] = queue.pop();
+  const [heatLoss, x, y, dirIndex, steps] = MinHeap.pop(queue);
   if (x === input[0].length - 1 && y === input.length - 1) {
     console.log(heatLoss);
     break;
@@ -34,20 +35,19 @@ while (queue.length) {
       continue;
     const newSteps = i === dirIndex ? steps + 1 : 1;
     const newHeatLoss = heatLoss + input[newY][newX];
-    const nextOne = [newX, newY, i, newSteps, newHeatLoss];
+    const nextOne = [newHeatLoss, newX, newY, i, newSteps];
     const state = stateKey(newX, newY, i, newSteps);
     if (visited.has(state) && visited.get(state) <= newHeatLoss) continue;
     visited.set(state, newHeatLoss);
-    queue.push(nextOne);
+    MinHeap.push(queue, nextOne);
   }
-  queue.sort((a, b) => b[4] - a[4]);
 }
 
-const queue2 = [[0, 0, -1, 5, 0]]; // use starting steps of 5 to make it choose a new direction
+const queue2 = [[0, 0, 0, -1, 5]]; // use starting steps of 5 to make it choose a new direction
 const visited2 = new Map();
 
 while (queue2.length) {
-  const [x, y, dirIndex, steps, heatLoss] = queue2.pop();
+  const [heatLoss, x, y, dirIndex, steps] = MinHeap.pop(queue2);
   if (x === input[0].length - 1 && y === input.length - 1) {
     console.log(heatLoss);
     break;
@@ -60,7 +60,7 @@ while (queue2.length) {
       continue;
     const newSteps = steps + 1;
     const newHeatLoss = heatLoss + input[newY][newX];
-    const nextOne = [newX, newY, dirIndex, newSteps, newHeatLoss];
+    const nextOne = [newHeatLoss, newX, newY, dirIndex, newSteps];
     const state = stateKey(newX, newY, dirIndex, newSteps);
     if (visited2.has(state) && visited2.get(state) <= newHeatLoss) continue;
     visited2.set(state, newHeatLoss);
@@ -80,12 +80,21 @@ while (queue2.length) {
         continue;
       const newSteps = i === dirIndex ? steps + 1 : 1;
       const newHeatLoss = heatLoss + input[newY][newX];
-      const nextOne = [newX, newY, i, newSteps, newHeatLoss];
+      const nextOne = [newHeatLoss, newX, newY, i, newSteps];
       const state = stateKey(newX, newY, i, newSteps);
       if (visited2.has(state) && visited2.get(state) <= newHeatLoss) continue;
       visited2.set(state, newHeatLoss);
-      queue2.push(nextOne);
+      MinHeap.push(queue2, nextOne);
     }
   }
-  queue2.sort((a, b) => b[4] - a[4]);
 }
+
+/*
+  Old solution:
+  part1: 6.462s
+  part2: 51.564s
+
+  New solution with minHeap:
+  part1: 298.899ms
+  part2: 752.789ms
+*/
