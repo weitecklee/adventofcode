bbbb0fe102c62b611e5004185f363e71ecc53c68
arkteck <arkteck@gmail.com>
2024-12-03 11:54:22 -0500

    Day 2024/3 in JS refactored

--- 2024/3/solution.js
+++ 2024/3/solution.js
@@ -5,35 +5,25 @@ const input = fs
   .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
   .split("\n");
 
-const mulRegex = /mul\((\d+),(\d+)\)/g;
+const regex = /mul\((\d+),(\d+)\)|do(n\'t)?\(\)/g;
 
 let part1 = 0;
-
-for (const line of input) {
-  let match;
-  while ((match = mulRegex.exec(line))) {
-    part1 += Number(match[1]) * Number(match[2]);
-  }
-}
-
-console.log(part1);
-
-const doOrMulRegex = /mul\((\d+),(\d+)\)|do(n\'t)?\(\)/g;
-
 let part2 = 0;
 
 let doMul = true;
 for (const line of input) {
   let match;
-  while ((match = doOrMulRegex.exec(line))) {
+  while ((match = regex.exec(line))) {
     if (match[0] === "do()") {
       doMul = true;
     } else if (match[0] === "don't()") {
       doMul = false;
     } else {
-      part2 += doMul ? Number(match[1]) * Number(match[2]) : 0;
+      const prod = Number(match[1]) * Number(match[2]);
+      part1 += prod;
+      part2 += doMul ? prod : 0;
     }
   }
 }
 
-console.log(part2);
+console.log(part1, part2);
