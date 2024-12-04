51355a46eeb75eec466a78a79d7694ed6eff6d6f
arkteck <arkteck@gmail.com>
2024-12-04 11:47:07 -0500

    Day 2023/24 Part 2 in JS

--- 2023/24/solution.js
+++ 2023/24/solution.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const mathjs = require("mathjs");
 
 const input = fs
   .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
@@ -40,3 +41,91 @@ for (let i = 0; i < hailstones.length; i++) {
 }
 
 console.log(part1);
+
+/*
+  Find some pos p and vel v such that for each hailstone with pos p_i and vel v_i, there exists a time t_i > 0 such that:
+    p[0] + v[0] * t_i = p_i[0] + v_i[0] * t_i
+    p[1] + v[1] * t_i = p_i[1] + v_i[1] * t_i
+    p[2] + v[2] * t_i = p_i[2] + v_i[2] * t_i
+
+  Rewrite to eliminate t_i:
+  t_i = (p[0] - p_i[0]) / (v_i[0] - v[0]) = (p[1] - p_i[1]) / (v_i[1] - v[1]) = (p[2] - p_i[2]) / (v_i[2] - v[2])
+  (p[0] - p_i[0]) * (v_i[1] - v[1]) = (p[1] - p_i[1]) * (v_i[0] - v[0])
+  (p[0] - p_i[0]) * (v_i[2] - v[2]) = (p[2] - p_i[2]) * (v_i[0] - v[0])
+
+  Expand:
+  p[0] * v_i[1] - p[0] * v[1] - p_i[0] * v_i[1] + p_i[0] * v[1] = p[1] * v_i[0] - p[1] * v[0] - p_i[1] * v_i[0] + p_i[1] * v[0]  // equation (a)
+  p[0] * v_i[2] - p[0] * v[2] - p_i[0] * v_i[2] + p_i[0] * v[2] = p[2] * v_i[0] - p[2] * v[0] - p_i[2] * v_i[0] + p_i[2] * v[0]  // equation (b)
+
+  If we introduce another hailstone with pos p_j and vel v_j, we get similarly:
+  p[0] * v_j[1] - p[0] * v[1] - p_j[0] * v_j[1] + p_j[0] * v[1] = p[1] * v_j[0] - p[1] * v[0] - p_j[1] * v_j[0] + p_j[1] * v[0]  // equation (c)
+  p[0] * v_j[2] - p[0] * v[2] - p_j[0] * v_j[2] + p_j[0] * v[2] = p[2] * v_j[0] - p[2] * v[0] - p_j[2] * v_j[0] + p_j[2] * v[0]  // equation (d)
+
+  Subtracting the pairs of equations (a/c and b/d) from each other eliminates the nonlinear terms (e.g., p[0] * v[1])
+  p[0] * v_i[1] - p[0] * v_j[1] - p_i[0] * v_i[1] + p_j[0] * v_j[1] + p_i[0] * v[1] - p_j[0] * v[1] = p[1] * v_i[0] - p[1] * v_j[0] - p_i[1] * v_i[0] + p_j[1] * v_j[0] + p_i[1] * v[0] - p_j[1] * v[0]
+  p[0] * v_i[2] - p[0] * v_j[2] - p_i[0] * v_i[2] + p_j[0] * v_j[2] + p_i[0] * v[2] - p_j[0] * v[2] = p[2] * v_i[0] - p[2] * v_j[0] - p_i[2] * v_i[0] + p_j[2] * v_j[0] + p_i[2] * v[0] - p_j[2] * v[0]
+
+  Rearrange to put unknowns on the left and knowns on the right:
+  p[0] * (v_i[1] - v_j[1]) + p[1] * (v_j[0] - v_i[0]) + v[0] * (p_j[1] - p_i[1]) + v[1] * (p_i[0] - p_j[0]) = p_i[0] * v_i[1] - p_j[0] * v_j[1] - p_i[1] * v_i[0] + p_j[1] * v_j[0]
+  p[0] * (v_i[2] - v_j[2]) + p[2] * (v_j[0] - v_i[0]) + v[0] * (p_j[2] - p_i[2]) + v[2] * (p_i[0] - p_j[0]) = p_i[0] * v_i[2] - p_j[0] * v_j[2] - p_i[2] * v_i[0] + p_j[2] * v_j[0]
+
+  Use four hailstone pairs to get six equations (i = 0, j = 1, 2, 3):
+  p[0] * (v_0[1] - v_1[1]) + p[1] * (v_1[0] - v_0[0]) + v[0] * (p_1[1] - p_0[1]) + v[1] * (p_0[0] - p_1[0]) = p_0[0] * v_0[1] - p_1[0] * v_1[1] - p_0[1] * v_0[0] + p_1[1] * v_1[0]
+  p[0] * (v_0[2] - v_1[2]) + p[2] * (v_1[0] - v_0[0]) + v[0] * (p_1[2] - p_0[2]) + v[2] * (p_0[0] - p_1[0]) = p_0[0] * v_0[2] - p_1[0] * v_1[2] - p_0[2] * v_0[0] + p_1[2] * v_1[0]
+  p[0] * (v_0[1] - v_2[1]) + p[1] * (v_2[0] - v_0[0]) + v[0] * (p_2[1] - p_0[1]) + v[1] * (p_0[0] - p_2[0]) = p_0[0] * v_0[1] - p_2[0] * v_2[1] - p_0[1] * v_0[0] + p_2[1] * v_2[0]
+  p[0] * (v_0[2] - v_2[2]) + p[2] * (v_2[0] - v_0[0]) + v[0] * (p_2[2] - p_0[2]) + v[2] * (p_0[0] - p_2[0]) = p_0[0] * v_0[2] - p_2[0] * v_2[2] - p_0[2] * v_0[0] + p_2[2] * v_2[0]
+  p[0] * (v_0[1] - v_3[1]) + p[1] * (v_3[0] - v_0[0]) + v[0] * (p_3[1] - p_0[1]) + v[1] * (p_0[0] - p_3[0]) = p_0[0] * v_0[1] - p_3[0] * v_3[1] - p_0[1] * v_0[0] + p_3[1] * v_3[0]
+  p[0] * (v_0[2] - v_3[2]) + p[2] * (v_3[0] - v_0[0]) + v[0] * (p_3[2] - p_0[2]) + v[2] * (p_0[0] - p_3[0]) = p_0[0] * v_0[2] - p_3[0] * v_3[2] - p_0[2] * v_0[0] + p_3[2] * v_3[0]
+
+  With that, we have six equations and six unknowns, no nonlinear terms so it can be solved with Cramer's rule.
+  Ax = b
+  x = [p[0], p[1], p[2], v[0], v[1], v[2]]T
+  A = [
+    [v_0[1] - v_1[1], v_1[0] - v_0[0],               0, p_1[1] - p_0[1], p_0[0] - p_1[0],               0],
+    [v_0[2] - v_1[2],               0, v_1[0] - v_0[0], p_1[2] - p_0[2],               0, p_0[0] - p_1[0]],
+    [v_0[1] - v_2[1], v_2[0] - v_0[0],               0, p_2[1] - p_0[1], p_0[0] - p_2[0],               0],
+    [v_0[2] - v_2[2],               0, v_2[0] - v_0[0], p_2[2] - p_0[2],               0, p_0[0] - p_2[0]],
+    [v_0[1] - v_3[1], v_3[0] - v_0[0],               0, p_3[1] - p_0[1], p_0[0] - p_3[0],               0],
+    [v_0[2] - v_3[2],               0, v_3[0] - v_0[0], p_3[2] - p_0[2],               0, p_0[0] - p_3[0]]
+  ]
+
+  b = [
+    p_0[0] * v_0[1] - p_1[0] * v_1[1] - p_0[1] * v_0[0] + p_1[1] * v_1[0],
+    p_0[0] * v_0[2] - p_1[0] * v_1[2] - p_0[2] * v_0[0] + p_1[2] * v_1[0],
+    p_0[0] * v_0[1] - p_2[0] * v_2[1] - p_0[1] * v_0[0] + p_2[1] * v_2[0],
+    p_0[0] * v_0[2] - p_2[0] * v_2[2] - p_0[2] * v_0[0] + p_2[2] * v_2[0],
+    p_0[0] * v_0[1] - p_3[0] * v_3[1] - p_0[1] * v_0[0] + p_3[1] * v_3[0],
+    p_0[0] * v_0[2] - p_3[0] * v_3[2] - p_0[2] * v_0[0] + p_3[2] * v_3[0]
+  ]
+  x = A^-1 * b
+*/
+
+const p_0 = hailstones[0].pos;
+const v_0 = hailstones[0].vel;
+const p_1 = hailstones[1].pos;
+const v_1 = hailstones[1].vel;
+const p_2 = hailstones[2].pos;
+const v_2 = hailstones[2].vel;
+const p_3 = hailstones[3].pos;
+const v_3 = hailstones[3].vel;
+
+const A = [
+  [v_0[1] - v_1[1], v_1[0] - v_0[0], 0, p_1[1] - p_0[1], p_0[0] - p_1[0], 0],
+  [v_0[2] - v_1[2], 0, v_1[0] - v_0[0], p_1[2] - p_0[2], 0, p_0[0] - p_1[0]],
+  [v_0[1] - v_2[1], v_2[0] - v_0[0], 0, p_2[1] - p_0[1], p_0[0] - p_2[0], 0],
+  [v_0[2] - v_2[2], 0, v_2[0] - v_0[0], p_2[2] - p_0[2], 0, p_0[0] - p_2[0]],
+  [v_0[1] - v_3[1], v_3[0] - v_0[0], 0, p_3[1] - p_0[1], p_0[0] - p_3[0], 0],
+  [v_0[2] - v_3[2], 0, v_3[0] - v_0[0], p_3[2] - p_0[2], 0, p_0[0] - p_3[0]],
+];
+
+const b = [
+  p_0[0] * v_0[1] - p_1[0] * v_1[1] - p_0[1] * v_0[0] + p_1[1] * v_1[0],
+  p_0[0] * v_0[2] - p_1[0] * v_1[2] - p_0[2] * v_0[0] + p_1[2] * v_1[0],
+  p_0[0] * v_0[1] - p_2[0] * v_2[1] - p_0[1] * v_0[0] + p_2[1] * v_2[0],
+  p_0[0] * v_0[2] - p_2[0] * v_2[2] - p_0[2] * v_0[0] + p_2[2] * v_2[0],
+  p_0[0] * v_0[1] - p_3[0] * v_3[1] - p_0[1] * v_0[0] + p_3[1] * v_3[0],
+  p_0[0] * v_0[2] - p_3[0] * v_3[2] - p_0[2] * v_0[0] + p_3[2] * v_3[0],
+];
+
+const x = mathjs.lusolve(mathjs.matrix(A), mathjs.matrix(b));
+console.log(x._data.slice(0, 3).reduce((a, b) => a + Math.round(Number(b)), 0));
