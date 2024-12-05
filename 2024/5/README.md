8a8512f8e00346a8d37aa950d53f3bb305ae8de0
arkteck <arkteck@gmail.com>
2024-12-05 09:38:06 -0500

    Day 2024/5 in JS

---
+++ 2024/5/README.md
@@ -0,0 +1,5 @@
+### Advent of Code 2024 Day 5
+
+[Puzzle Page](https://adventofcode.com/2024/day/5)
+
+[Solutions Megathread](https://www.reddit.com/r/adventofcode/comments/1h71eyz/2024_day_5_solutions/)
---
+++ 2024/5/solution.js
@@ -0,0 +1,82 @@
+const fs = require("fs");
+const path = require("path");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
+  .split("\n\n");
+
+const rules = input[0].split("\n").map((a) => a.split("|").map(Number));
+const updates = input[1].split("\n").map((a) => a.split(",").map(Number));
+
+class Rule {
+  constructor(num) {
+    this.num = num;
+    this.afterNum = new Set();
+  }
+
+  addRule(n) {
+    this.afterNum.add(n);
+  }
+}
+
+const ruleMap = new Map();
+
+for (const [n1, n2] of rules) {
+  if (!ruleMap.has(n1)) {
+    ruleMap.set(n1, new Rule(n1));
+  }
+  const rule = ruleMap.get(n1);
+  rule.addRule(n2);
+}
+
+function sortingFunction(a, b) {
+  if (ruleMap.has(a)) {
+    const rule = ruleMap.get(a);
+    if (rule.afterNum.has(b)) {
+      return -1;
+    } else {
+      return 0;
+    }
+  } else if (ruleMap.has(b)) {
+    const rule = ruleMap.get(b);
+    if (rule.afterNum.has(a)) {
+      return 1;
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+}
+
+let part1 = 0;
+let part2 = 0;
+
+for (const update of updates) {
+  const order = new Map();
+  for (let i = 0; i < update.length; i++) {
+    order.set(update[i], i);
+  }
+  let isCorrectOrder = true;
+  for (let i = 0; i < update.length; i++) {
+    if (ruleMap.has(update[i])) {
+      const rule = ruleMap.get(update[i]);
+      for (const n of rule.afterNum) {
+        if (order.has(n) && order.get(n) < i) {
+          isCorrectOrder = false;
+          break;
+        }
+      }
+    }
+    if (!isCorrectOrder) break;
+  }
+  if (isCorrectOrder) {
+    part1 += update[Math.floor(update.length / 2)];
+  } else {
+    update.sort(sortingFunction);
+    part2 += update[Math.floor(update.length / 2)];
+  }
+}
+
+console.log(part1);
+console.log(part2);
