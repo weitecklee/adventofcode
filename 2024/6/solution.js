fb6ac859bbc35d1a4148983334f49df03b7445fe
arkteck <arkteck@gmail.com>
2024-12-06 10:40:25 -0500

    refactor

--- 2024/6/solution.js
+++ 2024/6/solution.js
@@ -8,11 +8,16 @@ const input = fs
 
 class Guard {
   constructor() {
+    this.pos = [-1, -1];
     this.dir = [0, -1];
   }
 
   setPos(pos) {
-    this.pos = pos;
+    this.pos = pos.slice();
+  }
+
+  setDir(dir) {
+    this.dir = dir.slice();
   }
 
   turn() {
@@ -20,54 +25,60 @@ class Guard {
   }
 
   patrol(patrolArea = input) {
-    this.dir = [0, -1];
-    let [r, c] = this.pos;
     const patrolPath = new Set();
     const patrolPathWithDirection = new Set();
     while (
-      r >= 0 &&
-      r < patrolArea.length &&
-      c >= 0 &&
-      c < patrolArea[r].length
+      this.pos[1] >= 0 &&
+      this.pos[1] < patrolArea.length &&
+      this.pos[0] >= 0 &&
+      this.pos[0] < patrolArea[0].length
     ) {
-      patrolPath.add(`${r},${c}`);
+      patrolPath.add(`${this.pos[1]},${this.pos[0]}`);
       while (
-        r + this.dir[1] >= 0 &&
-        r + this.dir[1] < patrolArea.length &&
-        c + this.dir[0] >= 0 &&
-        c + this.dir[0] < patrolArea[r].length &&
-        patrolArea[r + this.dir[1]][c + this.dir[0]] === "#"
+        this.pos[1] + this.dir[1] >= 0 &&
+        this.pos[1] + this.dir[1] < patrolArea.length &&
+        this.pos[0] + this.dir[0] >= 0 &&
+        this.pos[0] + this.dir[0] < patrolArea[0].length &&
+        patrolArea[this.pos[1] + this.dir[1]][this.pos[0] + this.dir[0]] === "#"
       ) {
         this.turn();
       }
-      if (patrolPathWithDirection.has(`${r},${c},${this.dir.join(",")}`))
+      if (
+        patrolPathWithDirection.has(
+          `${this.pos[1]},${this.pos[0]},${this.dir[1]},${this.dir[0]}`
+        )
+      )
         return null;
-      patrolPathWithDirection.add(`${r},${c},${this.dir.join(",")}`);
-      r += this.dir[1];
-      c += this.dir[0];
+      patrolPathWithDirection.add(
+        `${this.pos[1]},${this.pos[0]},${this.dir[1]},${this.dir[0]}`
+      );
+      this.pos[1] += this.dir[1];
+      this.pos[0] += this.dir[0];
     }
     return patrolPath;
   }
 }
 
 const guard = new Guard();
-const obstructionRows = new Map();
-const obstructionCols = new Map();
 
 for (let r = 0; r < input.length; r++) {
   for (let c = 0; c < input[r].length; c++) {
     if (input[r][c] === "^") {
-      guard.setPos([r, c]);
+      guard.setPos([c, r]);
       break;
     }
   }
 }
+const origPos = guard.pos.slice();
 
 const patrolPath = guard.patrol();
 console.log(patrolPath.size);
 
+console.time("part2");
 let part2 = 0;
 for (const coord of patrolPath) {
+  guard.setPos(origPos);
+  guard.setDir([0, -1]);
   const [r, c] = coord.split(",").map(Number);
   input[r][c] = "#";
   const tmpPath = guard.patrol(input);
@@ -77,3 +88,6 @@ for (const coord of patrolPath) {
   input[r][c] = ".";
 }
 console.log(part2);
+console.timeEnd("part2");
+
+// Brute force solution : ~3.7s
