f6222eb4c935bfbe3b721060f8239f51eaa7e9e8
arkteck <arkteck@gmail.com>
2024-12-13 00:53:15 -0500

    refactor

--- 2024/13/solution.js
+++ 2024/13/solution.js
@@ -1,52 +1,56 @@
 const fs = require("fs");
 const path = require("path");
-const mathjs = require("mathjs");
 
 const input = fs
   .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
   .split("\n\n")
   .map((a) => a.split("\n"));
 
+function solve(A, b) {
+  const det = A[0][0] * A[1][1] - A[0][1] * A[1][0];
+  if (det === 0) {
+    return [0, 0];
+  }
+  return [
+    (b[0] * A[1][1] - b[1] * A[0][1]) / det,
+    (A[0][0] * b[1] - A[1][0] * b[0]) / det,
+  ];
+}
+
 class Machine {
   constructor(lines) {
     this.buttonA = lines[0].match(/\d+/g).map(Number);
     this.buttonB = lines[1].match(/\d+/g).map(Number);
     this.prize = lines[2].match(/\d+/g).map(Number);
+    this.prize2 = this.prize.map((a) => a + 10000000000000);
   }
 
   get tokenCost() {
-    const presses = mathjs
-      .lusolve(
-        mathjs.matrix([
-          [this.buttonA[0], this.buttonB[0]],
-          [this.buttonA[1], this.buttonB[1]],
-        ]),
-        mathjs.matrix(this.prize)
-      )
-      ._data.flat();
-    if (!presses.every((a) => Math.abs(a - Math.round(a)) < 1e-6)) {
-      return 0;
+    const [pressesA, pressesB] = solve(
+      [
+        [this.buttonA[0], this.buttonB[0]],
+        [this.buttonA[1], this.buttonB[1]],
+      ],
+      this.prize
+    );
+    if (Number.isInteger(pressesA) && Number.isInteger(pressesB)) {
+      return pressesA * 3 + pressesB;
     }
-    return presses[0] * 3 + presses[1];
+    return 0;
   }
 
   get tokenCost2() {
-    const presses = mathjs
-      .lusolve(
-        mathjs.matrix([
-          [this.buttonA[0], this.buttonB[0]],
-          [this.buttonA[1], this.buttonB[1]],
-        ]),
-        mathjs.matrix([
-          this.prize[0] + 10000000000000,
-          this.prize[1] + 10000000000000,
-        ])
-      )
-      ._data.flat();
-    if (!presses.every((a) => Math.abs(a - Math.round(a)) < 0.0001)) {
-      return 0;
+    const [pressesA, pressesB] = solve(
+      [
+        [this.buttonA[0], this.buttonB[0]],
+        [this.buttonA[1], this.buttonB[1]],
+      ],
+      this.prize2
+    );
+    if (Number.isInteger(pressesA) && Number.isInteger(pressesB)) {
+      return pressesA * 3 + pressesB;
     }
-    return presses[0] * 3 + presses[1];
+    return 0;
   }
 }
 
