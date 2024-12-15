c2315319a22397770f07025d3f0da63bcf1defcf
arkteck <arkteck@gmail.com>
2024-12-15 14:11:47 -0500

    Day 2024/15 Part 2 in JS

---
+++ 2024/15/README.md
@@ -0,0 +1,5 @@
+### Advent of Code 2024 Day 15
+
+[Puzzle Page](https://adventofcode.com/2024/day/15)
+
+[Solutions Megathread](https://www.reddit.com/r/adventofcode/comments/1hele8m/2024_day_15_solutions/)
--- 2024/15/solution.js
+++ 2024/15/solution.js
@@ -6,6 +6,13 @@ const input = fs
   .split("\n\n");
 
 const puzzleMap = input[0].split("\n").map((line) => line.split(""));
+const puzzleMap2 = input[0]
+  .replace(/#/g, "##")
+  .replace(/O/g, "[]")
+  .replace(/\./g, "..")
+  .replace(/@/g, "@.")
+  .split("\n")
+  .map((line) => line.split(""));
 const movements = input[1].split("\n").map((line) => line.split(""));
 
 const boxes = new Map();
@@ -42,6 +49,59 @@ class Robot {
     this.r += dr;
     this.c += dc;
   }
+
+  move2(chr) {
+    const [dr, dc] = directions.get(chr);
+    if (dc != 0) {
+      let [r, c] = [this.r, this.c + dc];
+      while (puzzleMap2[r][c] === "[" || puzzleMap2[r][c] === "]") {
+        c += dc;
+      }
+      if (puzzleMap2[r][c] === "#") return;
+      c -= dc;
+      while (puzzleMap2[r][c] === "[" || puzzleMap2[r][c] === "]") {
+        puzzleMap2[r][c - dc] = ".";
+        if (dc > 0) {
+          puzzleMap2[r][c] = "[";
+          puzzleMap2[r][c + dc] = "]";
+        } else {
+          puzzleMap2[r][c] = "]";
+          puzzleMap2[r][c + dc] = "[";
+        }
+        c -= 2 * dc;
+      }
+      puzzleMap2[r][c] = ".";
+      puzzleMap2[r][c + dc] = "@";
+      this.c += dc;
+    } else {
+      let [r, c] = [this.r + dr, this.c];
+      const affectedCols = [new Set([c])];
+      while (true) {
+        const newRange = new Set();
+        for (const col of affectedCols[affectedCols.length - 1]) {
+          if (puzzleMap2[r][col] === "#") return;
+          if (puzzleMap2[r][col] === "[") {
+            newRange.add(col);
+            newRange.add(col + 1);
+          } else if (puzzleMap2[r][col] === "]") {
+            newRange.add(col);
+            newRange.add(col - 1);
+          }
+        }
+        if (newRange.size === 0) break;
+        affectedCols.push(newRange);
+        r += dr;
+      }
+      for (let i = affectedCols.length - 1; i >= 0; i--) {
+        r -= dr;
+        for (const col of affectedCols[i]) {
+          puzzleMap2[r + dr][col] = puzzleMap2[r][col];
+          puzzleMap2[r][col] = ".";
+        }
+      }
+      this.r += dr;
+    }
+  }
 }
 
 const robot = new Robot();
@@ -70,3 +130,27 @@ for (let r = 0; r < puzzleMap.length; r++) {
 }
 
 console.log(part1);
+
+for (let r = 0; r < puzzleMap2.length; r++) {
+  for (let c = 0; c < puzzleMap2[r].length; c++) {
+    if (puzzleMap2[r][c] === "@") {
+      robot.r = r;
+      robot.c = c;
+    }
+  }
+}
+
+for (const line of movements) {
+  for (const movement of line) {
+    robot.move2(movement);
+  }
+}
+
+let part2 = 0;
+
+for (let r = 0; r < puzzleMap2.length; r++) {
+  for (let c = 0; c < puzzleMap2[r].length; c++) {
+    if (puzzleMap2[r][c] === "[") part2 += 100 * r + c;
+  }
+}
+console.log(part2);
