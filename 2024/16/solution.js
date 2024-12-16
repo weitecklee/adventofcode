4601de6eac7a838acbcffaff5d0859b5b68478e8
arkteck <arkteck@gmail.com>
2024-12-16 15:22:26 -0500

    Day 2024/16 Part 2 in JS

--- 2024/16/solution.js
+++ 2024/16/solution.js
@@ -25,16 +25,24 @@ for (let r = 0; r < input.length; r++) {
   }
 }
 
-const queue = [[0, ...start, 0]];
-const visitedScores = new Map([[`${start[0]},${start[1]}`, 0]]);
+const queue = [[0, ...start, 0, new Set([`${start[0]},${start[1]}`])]];
+const visitedScores = new Map([[`${start[0]},${start[1]},0`, 0]]);
+let part2 = new Set();
+let minScore = Infinity;
 
 while (queue.length) {
-  const [score, r, c, dirIndex] = MinHeap.pop(queue);
+  const [score, r, c, dirIndex, visited] = MinHeap.pop(queue);
   if (input[r][c] === "E") {
-    console.log(score);
-    break;
+    if (score <= minScore) {
+      minScore = score;
+      part2 = part2.union(visited);
+    } else {
+      break;
+    }
+    continue;
   }
   for (let i = 0; i < directions.length; i++) {
+    if (i === 3 - dirIndex) continue;
     const [dr, dc] = directions[i];
     const [newR, newC] = [r + dr, c + dc];
     if (
@@ -45,15 +53,19 @@ while (queue.length) {
       input[newR][newC] === "#"
     )
       continue;
-    const newScore =
-      score + 1 + (i === 3 - dirIndex ? 2000 : i !== dirIndex ? 1000 : 0);
+    const newScore = score + 1 + (i !== dirIndex ? 1000 : 0);
     if (
-      visitedScores.has(`${newR},${newC}`) &&
-      visitedScores.get(`${newR},${newC}`) <= newScore
+      visitedScores.has(`${newR},${newC},${i}`) &&
+      visitedScores.get(`${newR},${newC},${i}`) < newScore
     )
       continue;
-    visitedScores.set(`${newR},${newC}`, newScore);
-    const nextOne = [newScore, newR, newC, i];
+    visitedScores.set(`${newR},${newC},${i}`, newScore);
+    const newVisited = new Set(visited);
+    newVisited.add(`${newR},${newC}`);
+    const nextOne = [newScore, newR, newC, i, newVisited];
     MinHeap.push(queue, nextOne);
   }
 }
+
+console.log(minScore);
+console.log(part2.size);
