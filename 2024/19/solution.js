34d1b5134942dc077dba7480e57fda6af2224b7d
arkteck <arkteck@gmail.com>
2024-12-19 10:00:46 -0500

    refactor

--- 2024/19/solution.js
+++ 2024/19/solution.js
@@ -6,42 +6,29 @@ const input = fs
   .split("\n\n");
 
 const patterns = new Set(input[0].split(", "));
-const memo = new Map([["", true]]);
-const memo2 = new Map([["", 1]]);
+const designs = input[1].split("\n");
+const memo = new Map([["", 1]]);
 
-function recur(design) {
+function countWays(design) {
   if (memo.has(design)) return memo.get(design);
-  for (let i = 0; i < design.length; i++) {
-    if (patterns.has(design.slice(0, i + 1))) {
-      if (recur(design.slice(i + 1))) {
-        memo.set(design, true);
-        return true;
-      }
-    }
-  }
-  memo.set(design, false);
-  return false;
-}
-
-function recur2(design) {
-  if (memo2.has(design)) return memo2.get(design);
   let count = 0;
   for (let i = 0; i < design.length; i++) {
     if (patterns.has(design.slice(0, i + 1))) {
-      count += recur2(design.slice(i + 1));
+      count += countWays(design.slice(i + 1));
     }
   }
-  memo2.set(design, count);
+  memo.set(design, count);
   return count;
 }
 
 let part1 = 0;
 let part2 = 0;
-for (const design of input[1].split("\n")) {
-  if (recur(design)) {
-    part1++;
-    part2 += recur2(design);
-  }
+
+for (const design of designs) {
+  const ways = countWays(design);
+  part1 += ways > 0 ? 1 : 0;
+  part2 += ways;
 }
+
 console.log(part1);
 console.log(part2);
