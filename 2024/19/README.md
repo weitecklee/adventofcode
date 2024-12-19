03c73b8685ffdd39c50359f3ef08224066af7f3e
arkteck <arkteck@gmail.com>
2024-12-19 00:53:22 -0500

    Day 2024/19 Part 2 in JS

---
+++ 2024/19/README.md
@@ -0,0 +1,5 @@
+### Advent of Code 2024 Day 19
+
+[Puzzle Page](https://adventofcode.com/2024/day/19)
+
+[Solutions Megathread](https://www.reddit.com/r/adventofcode/comments/1hhlb8g/2024_day_19_solutions/)
--- 2024/19/solution.js
+++ 2024/19/solution.js
@@ -6,13 +6,13 @@ const input = fs
   .split("\n\n");
 
 const patterns = new Set(input[0].split(", "));
-const memo = new Map();
+const memo = new Map([["", true]]);
+const memo2 = new Map([["", 1]]);
 
 function recur(design) {
   if (memo.has(design)) return memo.get(design);
   for (let i = 0; i < design.length; i++) {
     if (patterns.has(design.slice(0, i + 1))) {
-      if (i === design.length - 1) return true;
       if (recur(design.slice(i + 1))) {
         memo.set(design, true);
         return true;
@@ -23,9 +23,25 @@ function recur(design) {
   return false;
 }
 
+function recur2(design) {
+  if (memo2.has(design)) return memo2.get(design);
+  let count = 0;
+  for (let i = 0; i < design.length; i++) {
+    if (patterns.has(design.slice(0, i + 1))) {
+      count += recur2(design.slice(i + 1));
+    }
+  }
+  memo2.set(design, count);
+  return count;
+}
+
 let part1 = 0;
+let part2 = 0;
 for (const design of input[1].split("\n")) {
-  const possible = recur(design);
-  if (possible) part1++;
+  if (recur(design)) {
+    part1++;
+    part2 += recur2(design);
+  }
 }
 console.log(part1);
+console.log(part2);
