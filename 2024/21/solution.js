df1cd1a136ddefcbf556d9178faa1f240a5f2b3a
arkteck <arkteck@gmail.com>
2025-01-06 11:41:17 -0500

    add comments

--- 2024/21/solution.js
+++ 2024/21/solution.js
@@ -5,10 +5,22 @@ const input = fs
   .readFileSync(path.join(__dirname, "input.txt"), "utf-8")
   .split("\n");
 
+/*
+  Cannot just return full movement string, quickly becomes untenable for part 2.
+  Instead, use a moveCounts map to keep track of how many times each movement (in
+  the form of "A->", "<-^", etc.) is made after each layer of robot.
+  For each movement, Keypad.maneuver(movement) will determine the optimal move string,
+  which is then converted to a moveCounts map and returned.
+  Keypad.typeIn(moveCounts) is then used to iterate on the current moveCounts map,
+  calculate the necessary moves and counts to simulate that current moveCounts map,
+  and return a new moveCounts map.
+*/
+
 function convertCodeToMoveCounts(code) {
-  let moveCounts = new Map();
+  const moveCounts = new Map();
   code.split("").forEach((c, i) => {
     if (i === 0) {
+      // always start with robot on "A" button
       moveCounts.set(`A-${c}`, 1);
     } else {
       moveCounts.set(
@@ -20,15 +32,15 @@ function convertCodeToMoveCounts(code) {
   return moveCounts;
 }
 
-class Pad {
+class Keypad {
   constructor() {
     this.pad;
-    this.keyPositions;
+    this.keyPositions = new Map();
     this.memo = new Map();
   }
 
   initialize() {
-    this.keyPositions = new Map();
+    // initialize keyPositions map
     for (let r = 0; r < this.pad.length; r++) {
       for (let c = 0; c < this.pad[0].length; c++) {
         this.keyPositions.set(this.pad[r][c], [r, c]);
@@ -37,13 +49,21 @@ class Pad {
   }
 
   maneuver(movement) {
-    // returns string of button presses for movement (e.g., "A-<", "0-1", etc.)
     if (this.memo.has(movement)) {
       return this.memo.get(movement);
     }
     const [curr, target] = movement.split("-");
     const [r, c] = this.keyPositions.get(curr);
     const [r2, c2] = this.keyPositions.get(target);
+    // Optimal move string is determined by 2 rules:
+    // 1. never change direction more than once (e.g., "<<^" or "^<<" are better than "<^<")
+    // 2. favor buttons farther away from "A"
+    //    UNLESS this causes robot to go over gap ("_" button).
+    // With the setup of the dirpad, "<" button is always favored most,
+    // followed by "v", then "^" and ">". Then there is a check to see if robot goes over gap.
+    // If there is, simply reverse the move string. This works because gap is always at a
+    // corner. If "<<^" causes robot to go over gap, go the reverse way "^<<" instead.
+    // Always end move string with "A".
     let moves = [];
     for (let i = c; i > c2; i--) {
       moves.push("<");
@@ -62,15 +82,15 @@ class Pad {
       moves = moves.reverse();
     moves.push("A");
     moves = moves.join("");
-    this.memo.set(movement, moves);
-    return moves;
+    const moveCounts = convertCodeToMoveCounts(moves);
+    this.memo.set(movement, moveCounts);
+    return moveCounts;
   }
 
   typeIn(moveCounts) {
     const newMoveCounts = new Map();
     for (const [movement, n] of moveCounts) {
-      const code = this.maneuver(movement);
-      const tmp = convertCodeToMoveCounts(code);
+      const tmp = this.maneuver(movement);
       for (const [mvmt, a] of tmp) {
         newMoveCounts.set(mvmt, (newMoveCounts.get(mvmt) || 0) + a * n);
       }
@@ -79,7 +99,7 @@ class Pad {
   }
 }
 
-class Numpad extends Pad {
+class Numpad extends Keypad {
   constructor() {
     super();
     this.pad = [
@@ -92,7 +112,7 @@ class Numpad extends Pad {
   }
 }
 
-class Keypad extends Pad {
+class Dirpad extends Keypad {
   constructor() {
     super();
     this.pad = [
@@ -104,14 +124,18 @@ class Keypad extends Pad {
 }
 
 const numpad = new Numpad();
-const keypad = new Keypad();
+const dirpad = new Dirpad();
 
-function solve(code, nRobots) {
+function solve(code, nDirectionalRobots) {
+  // make first moveCounts map
   let moveCounts = convertCodeToMoveCounts(code);
+  // iterate with numpad first
   moveCounts = numpad.typeIn(moveCounts);
-  for (let i = 0; i < nRobots; i++) {
-    moveCounts = keypad.typeIn(moveCounts);
+  // iterate for however many robots with directional keypads
+  for (let i = 0; i < nDirectionalRobots; i++) {
+    moveCounts = dirpad.typeIn(moveCounts);
   }
+  // calculate complexity
   return (
     Array.from(moveCounts.values()).reduce((a, b) => a + b) *
     Number(code.slice(0, 3))
