08df5e5673622223b0a7103acb48b37216a3c807
arkteck <arkteck@gmail.com>
2025-01-14 11:11:49 -0500

    Day 2021/22 Part 2 in Go

--- 2021/22/solution.go
+++ 2021/22/solution.go
@@ -18,24 +18,25 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	positions := parseInput(strings.Split(string(data), "\n"))
-	fmt.Println(part1(positions))
+	cuboids := parseInput(strings.Split(string(data), "\n"))
+	fmt.Println(part1(cuboids))
+	fmt.Println(part2(cuboids))
 }
 
-type Instruction struct {
-	turnOn bool
+type Cuboid struct {
+	state  bool
 	xRange [2]int
 	yRange [2]int
 	zRange [2]int
 }
 
-func parseInput(input []string) []Instruction {
-	instructions := []Instruction{}
+func parseInput(input []string) []Cuboid {
+	cuboids := []Cuboid{}
 	re := regexp.MustCompile(`-?\d+`)
 	for _, line := range input {
-		turnOn := true
+		state := true
 		if line[1] == 'f' {
-			turnOn = false
+			state = false
 		}
 		matches := re.FindAllString(line, -1)
 		nums := []int{}
@@ -43,47 +44,47 @@ func parseInput(input []string) []Instruction {
 			num, _ := strconv.Atoi(match)
 			nums = append(nums, num)
 		}
-		instructions = append(instructions, Instruction{
-			turnOn: turnOn,
+		cuboids = append(cuboids, Cuboid{
+			state:  state,
 			xRange: [2]int{nums[0], nums[1]},
 			yRange: [2]int{nums[2], nums[3]},
 			zRange: [2]int{nums[4], nums[5]},
 		})
 	}
-	return instructions
+	return cuboids
 }
 
-func part1(instructions []Instruction) int {
+func part1(cuboids []Cuboid) int {
 	cubes := map[[3]int]bool{}
-	for _, instruction := range instructions {
+	for _, cuboid := range cuboids {
 		xMin := -50
-		if instruction.xRange[0] > xMin {
-			xMin = instruction.xRange[0]
+		if cuboid.xRange[0] > xMin {
+			xMin = cuboid.xRange[0]
 		}
 		xMax := 50
-		if instruction.xRange[1] < xMax {
-			xMax = instruction.xRange[1]
+		if cuboid.xRange[1] < xMax {
+			xMax = cuboid.xRange[1]
 		}
 		yMin := -50
-		if instruction.yRange[0] > yMin {
-			yMin = instruction.yRange[0]
+		if cuboid.yRange[0] > yMin {
+			yMin = cuboid.yRange[0]
 		}
 		yMax := 50
-		if instruction.yRange[1] < yMax {
-			yMax = instruction.yRange[1]
+		if cuboid.yRange[1] < yMax {
+			yMax = cuboid.yRange[1]
 		}
 		zMin := -50
-		if instruction.zRange[0] > zMin {
-			zMin = instruction.zRange[0]
+		if cuboid.zRange[0] > zMin {
+			zMin = cuboid.zRange[0]
 		}
 		zMax := 50
-		if instruction.zRange[1] < zMax {
-			zMax = instruction.zRange[1]
+		if cuboid.zRange[1] < zMax {
+			zMax = cuboid.zRange[1]
 		}
 		for i := xMin; i <= xMax; i++ {
 			for j := yMin; j <= yMax; j++ {
 				for k := zMin; k <= zMax; k++ {
-					cubes[[3]int{i, j, k}] = instruction.turnOn
+					cubes[[3]int{i, j, k}] = cuboid.state
 				}
 			}
 		}
@@ -95,4 +96,48 @@ func part1(instructions []Instruction) int {
 		}
 	}
 	return count
+}
+
+func part2(cuboids []Cuboid) int {
+	var overlaps []Cuboid
+	for _, cuboid1 := range cuboids {
+		var tmp []Cuboid
+		if cuboid1.state {
+			tmp = append(tmp, cuboid1)
+		}
+		for _, cuboid2 := range overlaps {
+			xMin := max(cuboid1.xRange[0], cuboid2.xRange[0])
+			xMax := min(cuboid1.xRange[1], cuboid2.xRange[1])
+			if xMin > xMax {
+				continue
+			}
+			yMin := max(cuboid1.yRange[0], cuboid2.yRange[0])
+			yMax := min(cuboid1.yRange[1], cuboid2.yRange[1])
+			if yMin > yMax {
+				continue
+			}
+			zMin := max(cuboid1.zRange[0], cuboid2.zRange[0])
+			zMax := min(cuboid1.zRange[1], cuboid2.zRange[1])
+			if zMin > zMax {
+				continue
+			}
+			tmp = append(tmp, Cuboid{
+				state:  !cuboid2.state,
+				xRange: [2]int{xMin, xMax},
+				yRange: [2]int{yMin, yMax},
+				zRange: [2]int{zMin, zMax},
+			})
+		}
+		overlaps = append(overlaps, tmp...)
+	}
+	res := 0
+	for _, cuboid := range overlaps {
+		size := (cuboid.xRange[1] - cuboid.xRange[0] + 1) * (cuboid.yRange[1] - cuboid.yRange[0] + 1) * (cuboid.zRange[1] - cuboid.zRange[0] + 1)
+		if cuboid.state {
+			res += size
+		} else {
+			res -= size
+		}
+	}
+	return res
 }
