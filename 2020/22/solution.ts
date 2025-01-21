fadfb5dc28a2369db8a9d3a56fd7e874a35676af
arkteck <arkteck@gmail.com>
2025-01-21 11:20:22 -0500

    Day 2020/22 Part 2 in TS

--- 2020/22/solution.ts
+++ 2020/22/solution.ts
@@ -32,6 +32,10 @@ class Player {
   get score(): number {
     return this.deck.reduce((a, b, i) => a + b * (this.deck.length - i), 0);
   }
+
+  copy(n: number): Player {
+    return new Player(this.deck.slice(0, n));
+  }
 }
 
 const player1 = new Player(input[0].split("\n").slice(1).map(Number));
@@ -41,3 +45,37 @@ let winner = 0;
 while ((winner = player1.play(player2)) === 0) {}
 if (winner === 1) console.log(player1.score);
 else console.log(player2.score);
+
+function historyKey(player1: Player, player2: Player): string {
+  return player1.deck.join(",") + "|" + player2.deck.join(",");
+}
+
+function recursiveCombat(player1: Player, player2: Player): number {
+  const combatHistory: Set<string> = new Set();
+  while (true) {
+    const currKey = historyKey(player1, player2);
+    if (combatHistory.has(currKey)) {
+      return 1;
+    }
+    combatHistory.add(currKey);
+    const card1 = player1.draw();
+    const card2 = player2.draw();
+    if (player1.deck.length >= card1 && player2.deck.length >= card2) {
+      const winner = recursiveCombat(player1.copy(card1), player2.copy(card2));
+      if (winner === 1) player1.deck.push(card1, card2);
+      else player2.deck.push(card2, card1);
+    } else {
+      if (card1 > card2) player1.deck.push(card1, card2);
+      else player2.deck.push(card2, card1);
+    }
+    if (player1.deck.length === 0) return 2;
+    if (player2.deck.length === 0) return 1;
+  }
+}
+
+const player1Recur = new Player(input[0].split("\n").slice(1).map(Number));
+const player2Recur = new Player(input[1].split("\n").slice(1).map(Number));
+
+winner = recursiveCombat(player1Recur, player2Recur);
+if (winner === 1) console.log(player1Recur.score);
+else console.log(player2Recur.score);
