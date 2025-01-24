16eacffe76acbcb68ac6a139118c92cf9ae3ba39
arkteck <arkteck@gmail.com>
2025-01-24 09:55:03 -0500

    Day 2019/13 Part 2 in TS

--- 2019/13/solution.ts
+++ 2019/13/solution.ts
@@ -85,7 +85,7 @@ function* intcodeGenerator(prog: number[]): IntcodeGenerator {
         break;
       case 3:
         // save input
-        program.set(params[0], yield 9999);
+        program.set(params[0], yield -9999);
         i++;
         break;
       case 4:
@@ -152,10 +152,40 @@ let part1 = 0;
 
 while (true) {
   if (arcadeCabinet.next().done) break;
-  if (arcadeCabinet.next().done) break;
-  const tile = arcadeCabinet.next();
-  if (tile.done) break;
-  if (tile.value === 2) part1++;
+  arcadeCabinet.next();
+  const tileRet = arcadeCabinet.next();
+  if (tileRet.value === 2) part1++;
 }
 
 console.log(part1);
+
+const freePlayInput = puzzleInput.slice();
+freePlayInput[0] = 2;
+
+const freePlayArcade = intcodeGenerator(freePlayInput);
+let move = 0;
+let xBall = 0;
+let xPaddle = 0;
+let part2 = 0;
+while (true) {
+  let xRet: IteratorResult<number, number>;
+  while (true) {
+    xRet = freePlayArcade.next(move);
+    if (xRet.done || xRet.value === -9999) break;
+    const yRet = freePlayArcade.next();
+    const tileRet = freePlayArcade.next();
+    if (xRet.value === -1 && yRet.value === 0) {
+      part2 = tileRet.value;
+    }
+    if (tileRet.value === 4) {
+      xBall = xRet.value;
+    } else if (tileRet.value === 3) {
+      xPaddle = xRet.value;
+    }
+  }
+  if (xRet.done) break;
+  if (xBall < xPaddle) move = -1;
+  else if (xBall > xPaddle) move = 1;
+  else move = 0;
+}
+console.log(part2);
