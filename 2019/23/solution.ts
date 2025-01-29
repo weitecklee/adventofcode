70ee1b5df3b795183e90d0950f7714b4af014558
arkteck <arkteck@gmail.com>
2025-01-29 14:00:22 -0500

    refactor and comments

--- 2019/23/solution.ts
+++ 2019/23/solution.ts
@@ -11,19 +11,28 @@ const computers: Map<number, IntcodeGenerator> = new Map();
 
 let outbox: number[][] = [];
 
+// initialize computers with network addresses
 for (let i = 0; i < 50; i++) {
   const computer = intcodeGenerator(puzzleInput);
   computer.next();
   computer.next(i);
-  let ret = computer.next(-1);
-  while (ret.value != -9999) {
-    const dst = ret.value;
+  computers.set(i, computer);
+}
+
+function fillOutbox(
+  outbox: number[][],
+  computer: IntcodeGenerator,
+  lastRet: IteratorResult<number, number>
+) {
+  // fills outbox with output from computer until output is -9999
+  // (computer is done sending and is now requesting input)
+  while (lastRet.value != -9999) {
+    const dst = lastRet.value;
     const x = computer.next().value;
     const y = computer.next().value;
     outbox.push([dst, x, y]);
-    ret = computer.next();
+    lastRet = computer.next();
   }
-  computers.set(i, computer);
 }
 
 let i = 0;
@@ -32,40 +41,18 @@ let natPacket = [0, 0];
 let part1 = 0;
 const ySet = new Set();
 while (true) {
-  while (i < outbox.length) {
-    const [dst, x, y] = outbox[i];
-    i++;
-    if (dst === 255) {
-      natPacket = [x, y];
-      if (!part1) part1 = y;
-      continue;
-    }
-    const computer = computers.get(dst)!;
-    computer.next(x);
-    ret = computer.next(y);
-    while (ret.value != -9999) {
-      const dst = ret.value;
-      const x = computer.next().value;
-      const y = computer.next().value;
-      outbox.push([dst, x, y]);
-      ret = computer.next();
-    }
-  }
-  outbox = [];
-  i = 0;
+  // iterate through computers and fill outbox
   for (let j = 0; j < 50; j++) {
     const computer = computers.get(j)!;
     ret = computer.next(-1);
-    while (ret.value != -9999) {
-      const dst = ret.value;
-      const x = computer.next().value;
-      const y = computer.next().value;
-      outbox.push([dst, x, y]);
-      ret = computer.next();
-    }
+    fillOutbox(outbox, computer, ret);
   }
+
+  // if nothing in outbox, network is idle.
+  // NAT sends its last received packet to computer 0.
   if (outbox.length === 0) {
     if (ySet.has(natPacket[1])) {
+      // y value has been seen before, puzzle solved!
       console.log(part1);
       console.log(natPacket[1]);
       break;
@@ -73,4 +60,25 @@ while (true) {
     ySet.add(natPacket[1]);
     outbox = [[0, ...natPacket]];
   }
+
+  // iterate through outbox (outbox may grow during this process)
+  while (i < outbox.length) {
+    const [dst, x, y] = outbox[i];
+    i++;
+    if (dst === 255) {
+      // only keep track of last packet sent to 255
+      natPacket = [x, y];
+      // only keep track of first y value sent to 255
+      if (!part1) part1 = y;
+      continue;
+    }
+    const computer = computers.get(dst)!;
+    computer.next(x);
+    ret = computer.next(y);
+    fillOutbox(outbox, computer, ret);
+  }
+
+  // reset outbox
+  outbox = [];
+  i = 0;
 }
