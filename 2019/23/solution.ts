bfb040e9902e11bdbb8f8a57742f3cc6ef6b9e20
arkteck <arkteck@gmail.com>
2025-01-29 13:42:12 -0500

    Day 2019/23 Part 2 in TS

--- 2019/23/solution.ts
+++ 2019/23/solution.ts
@@ -9,7 +9,7 @@ const puzzleInput = fs
 
 const computers: Map<number, IntcodeGenerator> = new Map();
 
-const outbox: number[][] = [];
+let outbox: number[][] = [];
 
 for (let i = 0; i < 50; i++) {
   const computer = intcodeGenerator(puzzleInput);
@@ -27,22 +27,50 @@ for (let i = 0; i < 50; i++) {
 }
 
 let i = 0;
-let ret;
-while (i < outbox.length) {
-  const [dst, x, y] = outbox[i];
-  if (dst === 255) {
-    console.log(y);
-    break;
+let ret: IteratorResult<number, number>;
+let natPacket = [0, 0];
+let part1 = 0;
+const ySet = new Set();
+while (true) {
+  while (i < outbox.length) {
+    const [dst, x, y] = outbox[i];
+    i++;
+    if (dst === 255) {
+      natPacket = [x, y];
+      if (!part1) part1 = y;
+      continue;
+    }
+    const computer = computers.get(dst)!;
+    computer.next(x);
+    ret = computer.next(y);
+    while (ret.value != -9999) {
+      const dst = ret.value;
+      const x = computer.next().value;
+      const y = computer.next().value;
+      outbox.push([dst, x, y]);
+      ret = computer.next();
+    }
   }
-  i++;
-  const computer = computers.get(dst)!;
-  computer.next(x);
-  ret = computer.next(y);
-  while (ret.value != -9999) {
-    const dst = ret.value;
-    const x = computer.next().value;
-    const y = computer.next().value;
-    outbox.push([dst, x, y]);
-    ret = computer.next();
+  outbox = [];
+  i = 0;
+  for (let j = 0; j < 50; j++) {
+    const computer = computers.get(j)!;
+    ret = computer.next(-1);
+    while (ret.value != -9999) {
+      const dst = ret.value;
+      const x = computer.next().value;
+      const y = computer.next().value;
+      outbox.push([dst, x, y]);
+      ret = computer.next();
+    }
+  }
+  if (outbox.length === 0) {
+    if (ySet.has(natPacket[1])) {
+      console.log(part1);
+      console.log(natPacket[1]);
+      break;
+    }
+    ySet.add(natPacket[1]);
+    outbox = [[0, ...natPacket]];
   }
 }
