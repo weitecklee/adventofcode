9634c5fd24b54c3e4eb1d60856edeaf630350a00
arkteck <arkteck@gmail.com>
2025-01-31 12:33:17 -0500

    refactor

--- 2018/9/solution.ts
+++ 2018/9/solution.ts
@@ -5,36 +5,38 @@ const puzzleInput = fs.readFileSync(path.join(__dirname, "input.txt"), "utf-8");
 
 const [nPlayers, nMarbles] = puzzleInput.match(/\d+/g)!.map(Number);
 
-interface Marble {
+class Marble {
   value: number;
-  prev?: Marble;
-  next?: Marble;
+  next: Marble;
+  prev: Marble;
+  constructor(value: number) {
+    this.value = value;
+    this.next = this;
+    this.prev = this;
+  }
 }
 
 class Circle {
   currentMarble: Marble;
   constructor() {
-    const marble0: Marble = { value: 0 };
-    marble0.next = marble0;
-    marble0.prev = marble0;
-    this.currentMarble = marble0;
+    this.currentMarble = new Marble(0);
   }
 
   addMarble(n: number): number {
     if (n % 23 === 0) {
       let curr = this.currentMarble;
       for (let i = 0; i < 7; i++) {
-        curr = curr.prev!;
+        curr = curr.prev;
       }
-      curr.next!.prev = curr.prev;
-      curr.prev!.next = curr.next;
-      this.currentMarble = curr.next!;
+      curr.next.prev = curr.prev;
+      curr.prev.next = curr.next;
+      this.currentMarble = curr.next;
       return curr.value + n;
     }
 
-    const newMarble: Marble = { value: n };
-    const place = this.currentMarble.next!;
-    place.next!.prev = newMarble;
+    const newMarble = new Marble(n);
+    const place = this.currentMarble.next;
+    place.next.prev = newMarble;
     newMarble.next = place.next;
     newMarble.prev = place;
     place.next = newMarble;
@@ -43,16 +45,14 @@ class Circle {
   }
 }
 
-const circle = new Circle();
-const scores = Array(nPlayers).fill(0);
-for (let i = 1; i <= nMarbles; i++) {
-  scores[(i - 1) % nPlayers] += circle.addMarble(i);
+function play(nPlayers: number, nMarbles: number): number {
+  const circle = new Circle();
+  const scores = Array(nPlayers).fill(0);
+  for (let i = 1; i <= nMarbles; i++) {
+    scores[(i - 1) % nPlayers] += circle.addMarble(i);
+  }
+  return Math.max(...scores);
 }
-console.log(Math.max(...scores));
 
-const circle2 = new Circle();
-const scores2 = Array(nPlayers).fill(0);
-for (let i = 1; i <= nMarbles * 100; i++) {
-  scores2[(i - 1) % nPlayers] += circle2.addMarble(i);
-}
-console.log(Math.max(...scores2));
+console.log(play(nPlayers, nMarbles));
+console.log(play(nPlayers, nMarbles * 100));
