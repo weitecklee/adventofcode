dd7ceeec57ca41df1656d25afd3e0bb59790dcc7
arkteck <arkteck@gmail.com>
2025-01-31 11:54:11 -0500

    Day 2019/24 Part 2 in TS

--- 2019/24/solution.ts
+++ 2019/24/solution.ts
@@ -70,3 +70,76 @@ class Grid {
 const grid = new Grid(puzzleInput);
 while (!grid.iterate()) {}
 console.log(grid.biodiversityRating);
+
+class Hypergrid {
+  bugs: number[][];
+  constructor(input: string[][]) {
+    this.bugs = [];
+    for (let r = 0; r < input.length; r++) {
+      for (let c = 0; c < input[r].length; c++) {
+        if (input[r][c] === "#") this.bugs.push([r, c, 0]);
+      }
+    }
+  }
+
+  iterate() {
+    const neighborMap: Map<string, number> = new Map();
+    for (const [r, c, level] of this.bugs) {
+      const neighbors: number[][] = [];
+      for (const [dr, dc] of directions) {
+        const r2 = r + dr;
+        const c2 = c + dc;
+        if (r2 < 0) {
+          neighbors.push([1, 2, level - 1]);
+        } else if (r2 > 4) {
+          neighbors.push([3, 2, level - 1]);
+        } else if (c2 < 0) {
+          neighbors.push([2, 1, level - 1]);
+        } else if (c2 > 4) {
+          neighbors.push([2, 3, level - 1]);
+        } else if (r === 1 && c === 2 && dr === 1) {
+          for (let i = 0; i < 5; i++) {
+            neighbors.push([0, i, level + 1]);
+          }
+        } else if (r === 3 && c == 2 && dr == -1) {
+          for (let i = 0; i < 5; i++) {
+            neighbors.push([4, i, level + 1]);
+          }
+        } else if (r === 2 && c === 1 && dc === 1) {
+          for (let i = 0; i < 5; i++) {
+            neighbors.push([i, 0, level + 1]);
+          }
+        } else if (r === 2 && c == 3 && dc == -1) {
+          for (let i = 0; i < 5; i++) {
+            neighbors.push([i, 4, level + 1]);
+          }
+        } else {
+          neighbors.push([r2, c2, level]);
+        }
+      }
+      for (const posArr of neighbors) {
+        const key = posArr.join(",");
+        neighborMap.set(key, (neighborMap.get(key) || 0) + 1);
+      }
+    }
+    const bugs2: number[][] = [];
+    for (const bug of this.bugs) {
+      if ((neighborMap.get(bug.join(",")) || 0) === 1) {
+        bugs2.push(bug);
+      }
+      neighborMap.delete(bug.join(","));
+    }
+    for (const [pos, n] of neighborMap) {
+      if (n === 1 || n === 2) {
+        bugs2.push(pos.split(",").map(Number));
+      }
+    }
+    this.bugs = bugs2;
+  }
+}
+
+const hypergrid = new Hypergrid(puzzleInput);
+for (let i = 0; i < 200; i++) {
+  hypergrid.iterate();
+}
+console.log(hypergrid.bugs.length);
