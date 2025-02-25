6863c9fed4f9e9b2fedff895870e24f56f022c96
arkteck <arkteck@gmail.com>
2025-02-25 12:20:55 -0500

    Day 2019/9 in Go

---
+++ 2019/9/solution.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/weitecklee/adventofcode/2019/intcode"
+)
+
+func main() {
+	_, filename, _, _ := runtime.Caller(0)
+	dirname := filepath.Dir(filename)
+	inputFilePath := filepath.Join(dirname, "input.txt")
+	data, err := os.ReadFile(inputFilePath)
+	if err != nil {
+		panic(err)
+	}
+	puzzleInput := parseInput(strings.Split(string(data), ","))
+	fmt.Println(runBOOST(puzzleInput, 1))
+	fmt.Println(runBOOST(puzzleInput, 2))
+}
+
+func parseInput(data []string) []int {
+	numbers := make([]int, 0, len(data))
+	for _, s := range data {
+		if n, err := strconv.Atoi(s); err != nil {
+			panic(err)
+		} else {
+			numbers = append(numbers, n)
+		}
+	}
+	return numbers
+}
+
+func runBOOST(puzzleInput []int, initialInput int) int {
+	inChan := make(chan int)
+	defer close(inChan)
+	outChan := make(chan int)
+	defer close(outChan)
+	var wg sync.WaitGroup
+	ic := intcode.NewIntcodeProgram(puzzleInput, inChan, outChan, &wg)
+	wg.Add(1)
+	go ic.Run()
+	inChan <- initialInput
+	return <-outChan
+}
