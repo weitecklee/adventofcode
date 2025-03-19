c4c66c115f1a22a26869601e0c1ef2390fce1468
arkteck <arkteck@gmail.com>
2025-03-19 11:02:29 -0400

    Day 2024/8 Part 2 in Go

--- 2024/8/solution.go
+++ 2024/8/solution.go
@@ -16,12 +16,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	puzzleInput := strings.Split(string(data), "\n")
-	fmt.Println(part1(puzzleInput))
+	antennaMap, rMax, cMax := parseInput(strings.Split(string(data), "\n"))
+	fmt.Println(part1(antennaMap, rMax, cMax))
+	fmt.Println(part2(antennaMap, rMax, cMax))
 }
 
-func part1(puzzleInput []string) int {
-	antinodeMap := make(map[[2]int]struct{})
+func parseInput(puzzleInput []string) (map[byte][][2]int, int, int) {
 	antennaMap := make(map[byte][][2]int)
 	rMax := len(puzzleInput) - 1
 	cMax := len(puzzleInput[0]) - 1
@@ -32,6 +32,11 @@ func part1(puzzleInput []string) int {
 			}
 		}
 	}
+	return antennaMap, rMax, cMax
+}
+
+func countAntinodes(antennaMap map[byte][][2]int, rMax, cMax int, withResonantHarmonics bool) int {
+	antinodeMap := make(map[[2]int]struct{})
 	for _, pairs := range antennaMap {
 		for i, antenna1 := range pairs {
 			for _, antenna2 := range pairs[i+1:] {
@@ -39,14 +44,37 @@ func part1(puzzleInput []string) int {
 				dc := antenna2[1] - antenna1[1]
 				r1, c1 := antenna1[0]-dr, antenna1[1]-dc
 				r2, c2 := antenna2[0]+dr, antenna2[1]+dc
-				if r1 >= 0 && c1 >= 0 && r1 <= rMax && c1 <= cMax {
-					antinodeMap[[2]int{r1, c1}] = struct{}{}
-				}
-				if r2 >= 0 && c2 >= 0 && r2 <= rMax && c2 <= cMax {
-					antinodeMap[[2]int{r2, c2}] = struct{}{}
+				if withResonantHarmonics {
+					antinodeMap[antenna1] = struct{}{}
+					antinodeMap[antenna2] = struct{}{}
+					for r1 >= 0 && c1 >= 0 && r1 <= rMax && c1 <= cMax {
+						antinodeMap[[2]int{r1, c1}] = struct{}{}
+						r1 -= dr
+						c1 -= dc
+					}
+					for r2 >= 0 && c2 >= 0 && r2 <= rMax && c2 <= cMax {
+						antinodeMap[[2]int{r2, c2}] = struct{}{}
+						r2 += dr
+						c2 += dc
+					}
+				} else {
+					if r1 >= 0 && c1 >= 0 && r1 <= rMax && c1 <= cMax {
+						antinodeMap[[2]int{r1, c1}] = struct{}{}
+					}
+					if r2 >= 0 && c2 >= 0 && r2 <= rMax && c2 <= cMax {
+						antinodeMap[[2]int{r2, c2}] = struct{}{}
+					}
 				}
 			}
 		}
 	}
 	return len(antinodeMap)
+}
+
+func part1(antennaMap map[byte][][2]int, rMax, cMax int) int {
+	return countAntinodes(antennaMap, rMax, cMax, false)
+}
+
+func part2(antennaMap map[byte][][2]int, rMax, cMax int) int {
+	return countAntinodes(antennaMap, rMax, cMax, true)
 }
