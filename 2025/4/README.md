54d57b2319d19efa7b23cabd9f1e0bc30ac86ffa
arkteck <arkteck@gmail.com>
2025-12-04 10:54:30 -0500

    Day 2025/4 in Go

---
+++ 2025/4/README.md
@@ -0,0 +1,5 @@
+### Advent of Code 2025 Day 4
+
+[Puzzle Page](https://adventofcode.com/2025/day/4)
+
+[Solutions Megathread](https://www.reddit.com/r/adventofcode/comments/1pdr8x6/2025_day_4_solutions/)
---
+++ 2025/4/solution.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	_, filename, _, _ := runtime.Caller(0)
+	dirname := filepath.Dir(filename)
+	inputFilePath := filepath.Join(dirname, "input.txt")
+	data, err := os.ReadFile(inputFilePath)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(part1(parseInput(strings.Split(string(data), "\n"))))
+	fmt.Println(part2(parseInput(strings.Split(string(data), "\n"))))
+}
+
+func parseInput(data []string) map[[2]int]int {
+	papers := make(map[[2]int]int)
+	for i, row := range data {
+		for j, ch := range row {
+			if ch == '@' {
+				papers[[2]int{i, j}] = 0
+			}
+		}
+	}
+	return papers
+}
+
+func removePaper(papers map[[2]int]int) (int, map[[2]int]int) {
+	for coord := range papers {
+		papers[coord] = 0
+	}
+	for coord := range papers {
+		a, b := coord[0], coord[1]
+		for r := a - 1; r <= a+1; r++ {
+			for c := b - 1; c <= b+1; c++ {
+				if r == a && c == b {
+					continue
+				}
+				tmp := [2]int{r, c}
+				if _, ok := papers[tmp]; ok {
+					papers[tmp] += 1
+				}
+			}
+		}
+	}
+	removed := 0
+	for coord, ct := range papers {
+		if ct < 4 {
+			removed += 1
+			delete(papers, coord)
+		}
+	}
+	return removed, papers
+}
+
+func part1(puzzleInput map[[2]int]int) int {
+	res, _ := removePaper(puzzleInput)
+	return res
+}
+
+func part2(puzzleInput map[[2]int]int) int {
+	res := 0
+	var removed int
+	papers := puzzleInput
+	for {
+		removed, papers = removePaper(papers)
+		if removed == 0 {
+			break
+		}
+		res += removed
+	}
+	return res
+}
