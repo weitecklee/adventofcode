be5fce5bf8ee16a8f8bbf09db120fa30aff9b4fe
arkteck <arkteck@gmail.com>
2025-12-08 12:01:37 -0500

    refactor

--- 2025/8/solution.go
+++ 2025/8/solution.go
@@ -23,36 +23,31 @@ func main() {
 	fmt.Println(solve(puzzleInput))
 }
 
-type juncBox struct {
-	pos  [3]int
-	circ circuit
-}
-
-type circuit = map[*juncBox]struct{}
+type juncBox [3]int
+type circuit map[*juncBox]struct{}
 
 func parseInput(data []string) []*juncBox {
 	points := make([]*juncBox, len(data))
 	for i, line := range data {
-		points[i] = &juncBox{
-			[3]int(utils.ExtractInts(line)),
-			make(circuit),
-		}
-		points[i].circ[points[i]] = struct{}{}
+		box := juncBox(utils.ExtractInts(line))
+		points[i] = &box
 	}
 	return points
 }
 
 func calcDistance(point1, point2 *juncBox) int {
 	res := 0
-	for i := range point1.pos {
-		res += utils.PowInt(point1.pos[i]-point2.pos[i], 2)
+	for i := range point1 {
+		res += utils.PowInt(point1[i]-point2[i], 2)
 	}
 	return res
 }
 
-func connectBoxes(box1, box2 *juncBox, circuits []*circuit) []*circuit {
+func connectBoxes(box1, box2 *juncBox, circuits *[]*circuit) {
 	var circ1, circ2 *circuit
-	for _, circ := range circuits {
+	circs := *circuits
+
+	for _, circ := range circs {
 		if _, ok := (*circ)[box1]; ok {
 			circ1 = circ
 		}
@@ -63,11 +58,12 @@ func connectBoxes(box1, box2 *juncBox, circuits []*circuit) []*circuit {
 			break
 		}
 	}
+
 	if circ1 == nil && circ2 == nil {
 		circ := make(circuit)
 		circ[box1] = struct{}{}
 		circ[box2] = struct{}{}
-		circuits = append(circuits, &circ)
+		circs = append(circs, &circ)
 	} else if circ1 != nil && circ2 == nil {
 		(*circ1)[box2] = struct{}{}
 	} else if circ1 == nil && circ2 != nil {
@@ -77,14 +73,15 @@ func connectBoxes(box1, box2 *juncBox, circuits []*circuit) []*circuit {
 		for box := range *circ2 {
 			(*circ1)[box] = struct{}{}
 		}
-		for i, circ := range circuits {
+		for i, circ := range circs {
 			if circ == circ2 {
-				circuits = append(circuits[:i], circuits[i+1:]...)
+				circs = append(circs[:i], circs[i+1:]...)
 				break
 			}
 		}
 	}
-	return circuits
+
+	*circuits = circs
 }
 
 func solve(puzzleInput []*juncBox) (int, int) {
@@ -98,15 +95,18 @@ func solve(puzzleInput []*juncBox) (int, int) {
 			pairs = append(pairs, &pair)
 		}
 	}
+
 	slices.SortFunc(pairs, func(a, b *[2]*juncBox) int {
 		return distances[a] - distances[b]
 	})
+
 	circuits := make([]*circuit, 0, 1000)
 	for range 1000 {
 		pair := pairs[0]
 		pairs = pairs[1:]
-		circuits = connectBoxes(pair[0], pair[1], circuits)
+		connectBoxes(pair[0], pair[1], &circuits)
 	}
+
 	sizes := make([]int, len(circuits))
 	for i, circ := range circuits {
 		sizes[i] = len(*circ)
@@ -116,15 +116,17 @@ func solve(puzzleInput []*juncBox) (int, int) {
 	for i := 1; i <= 3; i++ {
 		part1 *= sizes[len(sizes)-i]
 	}
+
 	var part2 int
 	for {
 		pair := pairs[0]
 		pairs = pairs[1:]
-		circuits = connectBoxes(pair[0], pair[1], circuits)
+		connectBoxes(pair[0], pair[1], &circuits)
 		if len(*circuits[0]) == 1000 {
-			part2 = pair[0].pos[0] * pair[1].pos[0]
+			part2 = pair[0][0] * pair[1][0]
 			break
 		}
 	}
+
 	return part1, part2
 }
