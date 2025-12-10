5d3b7fdc2b23edfae6a1442761c86beacf0640c4
arkteck <arkteck@gmail.com>
2025-12-10 11:57:00 -0500

    refactor 2025/8 to use UnionFind

--- 2025/8/solution.go
+++ 2025/8/solution.go
@@ -24,18 +24,61 @@ func main() {
 }
 
 type juncBox [3]int
-type circuit map[*juncBox]struct{}
 
-func parseInput(data []string) []*juncBox {
-	points := make([]*juncBox, len(data))
+type Pair struct {
+	a, b, dist int
+}
+
+type UnionFind struct {
+	parent []int
+	size   []int
+}
+
+func (uf *UnionFind) FindParent(n int) int {
+	if uf.parent[n] != n {
+		uf.parent[n] = uf.FindParent(uf.parent[n])
+	}
+	return uf.parent[n]
+}
+
+func (uf *UnionFind) Union(a, b int) {
+	rootA := uf.FindParent(a)
+	rootB := uf.FindParent(b)
+
+	if rootA == rootB {
+		return
+	}
+
+	if uf.size[rootA] < uf.size[rootB] {
+		rootA, rootB = rootB, rootA
+	}
+
+	uf.parent[rootB] = rootA
+	uf.size[rootA] += uf.size[rootB]
+
+}
+
+func NewUnionFind(n int) *UnionFind {
+	uf := UnionFind{
+		make([]int, n),
+		make([]int, n),
+	}
+	for i := range uf.parent {
+		uf.parent[i] = i
+		uf.size[i] = 1
+	}
+	return &uf
+}
+
+func parseInput(data []string) []juncBox {
+	points := make([]juncBox, len(data))
 	for i, line := range data {
-		box := juncBox(utils.ExtractInts(line))
-		points[i] = &box
+		points[i] = juncBox(utils.ExtractInts(line))
 	}
 	return points
 }
 
-func calcDistance(point1, point2 *juncBox) int {
+func calcDistance(point1, point2 juncBox) int {
 	res := 0
 	for i := range point1 {
 		res += utils.PowInt(point1[i]-point2[i], 2)
@@ -43,89 +86,52 @@ func calcDistance(point1, point2 *juncBox) int {
 	return res
 }
 
-func connectBoxes(box1, box2 *juncBox, circuits *[]*circuit) {
-	var circ1, circ2 *circuit
-	circs := *circuits
-
-	for _, circ := range circs {
-		if _, ok := (*circ)[box1]; ok {
-			circ1 = circ
-		}
-		if _, ok := (*circ)[box2]; ok {
-			circ2 = circ
-		}
-		if circ1 != nil && circ2 != nil {
-			break
-		}
-	}
-
-	if circ1 == nil && circ2 == nil {
-		circ := make(circuit)
-		circ[box1] = struct{}{}
-		circ[box2] = struct{}{}
-		circs = append(circs, &circ)
-	} else if circ1 != nil && circ2 == nil {
-		(*circ1)[box2] = struct{}{}
-	} else if circ1 == nil && circ2 != nil {
-		(*circ2)[box1] = struct{}{}
-	} else if circ1 == circ2 {
-	} else {
-		for box := range *circ2 {
-			(*circ1)[box] = struct{}{}
-		}
-		for i, circ := range circs {
-			if circ == circ2 {
-				circs = append(circs[:i], circs[i+1:]...)
-				break
-			}
-		}
-	}
-
-	*circuits = circs
-}
-
-func solve(puzzleInput []*juncBox) (int, int) {
-	distances := make(map[*[2]*juncBox]int)
-	pairs := make([]*[2]*juncBox, 0, len(puzzleInput)*(len(puzzleInput)-1)/2)
+func solve(puzzleInput []juncBox) (int, int) {
+	pairs := make([]Pair, 0, len(puzzleInput)*(len(puzzleInput)-1)/2)
 	for i, box1 := range puzzleInput {
-		for _, box2 := range puzzleInput[i+1:] {
+		for j, box2 := range puzzleInput[i+1:] {
 			dist := calcDistance(box1, box2)
-			pair := [2]*juncBox{box1, box2}
-			distances[&pair] = dist
-			pairs = append(pairs, &pair)
+			pair := Pair{i, i + j + 1, dist}
+			pairs = append(pairs, pair)
 		}
 	}
 
-	slices.SortFunc(pairs, func(a, b *[2]*juncBox) int {
-		return distances[a] - distances[b]
+	uf := NewUnionFind(1000)
+	slices.SortFunc(pairs, func(a, b Pair) int {
+		return a.dist - b.dist
 	})
 
-	circuits := make([]*circuit, 0, 1000)
-	for range 1000 {
-		pair := pairs[0]
-		pairs = pairs[1:]
-		connectBoxes(pair[0], pair[1], &circuits)
+	for i := range 1000 {
+		pair := pairs[i]
+		uf.Union(pair.a, pair.b)
 	}
 
-	sizes := make([]int, len(circuits))
-	for i, circ := range circuits {
-		sizes[i] = len(*circ)
+	sizeMap := make(map[int]int)
+	for _, p := range uf.parent {
+		sizeMap[p] = uf.size[p]
+	}
+
+	sizes := make([]int, 0, len(sizeMap))
+	for _, s := range sizeMap {
+		sizes = append(sizes, s)
 	}
 	slices.Sort(sizes)
+
 	part1 := 1
 	for i := 1; i <= 3; i++ {
 		part1 *= sizes[len(sizes)-i]
 	}
 
 	var part2 int
+	i := 1000
 	for {
-		pair := pairs[0]
-		pairs = pairs[1:]
-		connectBoxes(pair[0], pair[1], &circuits)
-		if len(*circuits[0]) == 1000 {
-			part2 = pair[0][0] * pair[1][0]
+		pair := pairs[i]
+		uf.Union(pair.a, pair.b)
+		if uf.size[uf.FindParent(pair.a)] == 1000 {
+			part2 = puzzleInput[pair.a][0] * puzzleInput[pair.b][0]
 			break
 		}
+		i += 1
 	}
 
 	return part1, part2
