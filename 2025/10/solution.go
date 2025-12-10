97a727ebc8b9e32f96c60eebe52a31cd0a05a5ff
arkteck <arkteck@gmail.com>
2025-12-10 11:12:38 -0500

    Day 2025/10 Part 2 in Go

--- 2025/10/solution.go
+++ 2025/10/solution.go
@@ -7,8 +7,10 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"slices"
 	"strings"
 
+	"github.com/draffensperger/golp"
 	"github.com/weitecklee/adventofcode/utils"
 )
 
@@ -22,15 +24,17 @@ func main() {
 	}
 	machines := parseInput(strings.Split(string(data), "\n"))
 	fmt.Println(part1(machines))
+	fmt.Println(part2(machines))
 }
 
 type Machine struct {
 	lightDiagram int
-	buttons      []int
+	buttons      [][]int
+	buttonInts   []int
 	joltageReqs  []int
 }
 
-func NewMachine2(line string) *Machine {
+func NewMachine(line string) *Machine {
 	parts := strings.Split(line, " ")
 	var lightDiagram int
 	for i, ch := range parts[0][1 : len(parts[0])-1] {
@@ -38,12 +42,14 @@ func NewMachine2(line string) *Machine {
 			lightDiagram += 1 << i
 		}
 	}
-	buttons := make([]int, len(parts)-2)
+	buttons := make([][]int, len(parts)-2)
+	buttonInts := make([]int, len(parts)-2)
 
 	for i, s := range parts[1 : len(parts)-1] {
 		nums := utils.ExtractInts(s)
+		buttons[i] = nums
 		for _, n := range nums {
-			buttons[i] += 1 << n
+			buttonInts[i] += 1 << n
 		}
 	}
 
@@ -52,6 +58,7 @@ func NewMachine2(line string) *Machine {
 	m := Machine{
 		lightDiagram,
 		buttons,
+		buttonInts,
 		joltageReqs,
 	}
 
@@ -95,7 +102,7 @@ func (m *Machine) fewestPressesForLights() int {
 			continue
 		}
 
-		for _, button := range m.buttons {
+		for _, button := range m.buttonInts {
 			heap.Push(queue, &utils.Item[LightValue]{
 				Priority: presses + 1,
 				Value:    LightValue{lights ^ button, presses + 1},
@@ -105,10 +112,59 @@ func (m *Machine) fewestPressesForLights() int {
 	return res
 }
 
+/*
+	Use external golp package for solving LP equations (that's actually a wrapper
+	for lp_solve C library).
+	Each machine is a LP problem where objective is to minimize the number of
+	button presses.
+	Objective function is just slice of 1's with same length as number of buttons.
+	Default behavior of solver is already to minimize	objective function.
+	For each button, we also add integer constraint.
+	We add constraint for each joltage requirement. We use AddConstraintSparse
+	because we only need to add buttons that actually affect that joltage counter.
+	The constraint then has to be equal to the joltage requirement.
+	Finally, solve it!
+*/
+
+func (m *Machine) fewestPressesForJoltage() int {
+	lp := golp.NewLP(0, len(m.buttons))
+
+	objFn := make([]float64, len(m.buttons))
+	for i := range m.buttons {
+		lp.SetInt(i, true)
+		objFn[i] = 1.0
+	}
+	lp.SetObjFn(objFn)
+
+	for i, joltage := range m.joltageReqs {
+		entries := make([]golp.Entry, 0, len(m.buttons))
+		for b, button := range m.buttons {
+			if slices.Contains(button, i) {
+				entries = append(entries, golp.Entry{
+					Col: b,
+					Val: 1.0,
+				})
+			}
+		}
+		lp.AddConstraintSparse(entries, golp.EQ, float64(joltage))
+	}
+
+	lp.Solve()
+
+	presses := lp.Variables()
+
+	res := 0
+	for _, n := range presses {
+		res += int(n)
+	}
+
+	return res
+}
+
 func parseInput(data []string) []*Machine {
 	res := make([]*Machine, len(data))
 	for i, line := range data {
-		res[i] = NewMachine2(line)
+		res[i] = NewMachine(line)
 	}
 	return res
 }
@@ -117,6 +173,14 @@ func part1(machines []*Machine) int {
 	res := 0
 	for _, m := range machines {
 		res += m.fewestPressesForLights()
+	}
+	return res
+}
+
+func part2(machines []*Machine) int {
+	res := 0
+	for _, m := range machines {
+		res += m.fewestPressesForJoltage()
 	}
 	return res
 }
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.23
 toolchain go1.23.1
 
 require golang.org/x/oauth2 v0.24.0
+
+require github.com/draffensperger/golp v0.0.0-20250721104811-2d405f0b4e68 // indirect
